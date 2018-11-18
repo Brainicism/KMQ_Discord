731d73ce42c57b808689b0cfa8435fee6f208d79
Taaha Mahdi <mahditaaha11@gmail.com>
2018-11-17 23:37:10 -0500

    Added embeds for scoreboard/links

--- kmq.js
+++ kmq.js
@@ -11,34 +11,90 @@ const db = new sqlite3.Database('./main.db', (err) => {
         return;
     }
 });
-var scoreboard = {};
+var scoreboard = [];
 var currentSong = null;
 var currentArtist = null;
+var currentSongLink = null;
 var gameInSession = false;
+
+const songInfoMessage = (message, currentSong, currentArtist, currentSongLink, isQuit) => {
+    message.channel.send({embed: {
+        color: 15158332, // Red
+        author: {
+            name: isQuit ? null : message.author.username,
+            icon_url: isQuit ? null : message.author.avatarURL
+        },
+        title: currentSong + " - " + currentArtist,
+        description: "https://youtube.com/watch?v=" + currentSongLink,
+        image: {
+            url: "https://img.youtube.com/vi/" + currentSongLink + "/hqdefault.jpg"
+        },
+        footer: {
+            icon_url: client.user.avatarURL,
+            text: "i love kpop"
+        }
+    }})
+}
+
+const sendScoreboard = (message, scoreboard) => {
+    message.channel.send({embed: {
+        color: 15158332,
+        title: "**Results**",
+        fields: scoreboard
+    }})
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
+
 client.on('message', message => {
     if (message.author.equals(client.user)) return;
     let command = parseCommand(message.content) || null;
     if (command) {
         if (command.action === "stop") {
             gameInSession = false;
-            message.channel.send("The correct song was: " + currentSong + " by " + currentArtist);
+            songInfoMessage(message, currentSong, currentArtist, currentSongLink, true);
             disconnectVoiceConnection(message);
         }
         else if (command.action === "random") {
             startGame(message);
+        }
+        else if (command.action === "end") {
+            if (scoreboard.length > 0) {
+                disconnectVoiceConnection(message);
+                message.channel.send(scoreboard[0].name + " wins!");
+                sendScoreboard(message, scoreboard);
+                scoreboard = [];
+            }
         }
     }
     else {
         let guess = cleanSongName(message.content);
         if (currentSong && guess === cleanSongName(currentSong)) {
             //this should be atomic
-            scoreboard[getUserIdentifier(message.author)] = (scoreboard[getUserIdentifier(message.author)] || 0) + 1;
-            message.channel.send("Correct answer was: " + currentSong + " by " + currentArtist + "\n" + JSON.stringify(scoreboard));
+            let index = -1;
+            for (let i = 0; i < scoreboard.length; i++) {
+                if (scoreboard[i].name === getUserIdentifier(message.author)) {
+                    index = i;
+                }
+            }
+
+            if (index === -1) {
+                // Either scoreboard is empty or user isn't in scoreboard
+                scoreboard.push({name: getUserIdentifier(message.author), value: 1});
+            }
+
+            else {
+                scoreboard[index].value++;
+            }
+
+            songInfoMessage(message, currentSong, currentArtist, currentSongLink, false);
+            sendScoreboard(message, scoreboard);
             gameInSession = false;
             currentSong = null;
+            currentArtist = null
+            currentSongLink = null;
             disconnectVoiceConnection(message);
         }
     }
@@ -56,9 +112,10 @@ const startGame = (message) => {
         let random = rows[Math.floor(Math.random() * rows.length)];
         currentSong = random.name;
         currentArtist = random.artist;
+        currentSongLink = random.youtube_link;
         console.log(currentSong);
-        fetchVideoInfo(random.youtube_link, (err, videoInfo) => {
-            playSong(random.youtube_link, videoInfo.duration, message);
+        fetchVideoInfo(currentSongLink, (err, videoInfo) => {
+            playSong(currentSongLink, videoInfo.duration, message);
         })
     })
 }
@@ -71,6 +128,7 @@ const disconnectVoiceConnection = (message) => {
     }
     message.channel.send("no vc");
 }
+
 const playSong = (link, duration, message) => {
     var voiceChannel = message.member.voiceChannel;
     const streamOptions = { volume: 0.1 };
@@ -92,6 +150,7 @@ const parseCommand = (message) => {
         message
     }
 }
+
 const cleanSongName = (name) => {
     return name.toLowerCase().split("(")[0].replace(/[^\x00-\x7F|]/g, "").replace(/|/g, "").replace(/ /g, "").trim();
 }
@@ -99,4 +158,5 @@ const cleanSongName = (name) => {
 const getUserIdentifier = (user) => {
     return `${user.username}#${user.discriminator}`
 }
+
 client.login(config.bot_token);
