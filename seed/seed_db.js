0526a62483201ad2fcb68d80511b19a016996af3
Brainicism <brainicism@example.com>
2020-02-12 00:10:39 -0500

    Update database seed script, update dependencies

--- seed/seed_db.js
+++ seed/seed_db.js
@@ -1,6 +1,4 @@
 const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
-const parse = require('csv-parse/lib/sync')
 const request = require('request');
 const db = new sqlite3.Database('./main.db', (err) => {
     if (err) {
@@ -11,43 +9,54 @@ const db = new sqlite3.Database('./main.db', (err) => {
 
 });
 
+
+var headers = {
+    'authority': 'dbkpop.com',
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'origin': 'https://dbkpop.com',
+    'x-requested-with': 'XMLHttpRequest',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'referer': 'https://dbkpop.com/db/k-pop-music-videos'
+};
+
+var dataString = 'draw=1&columns%5B0%5D%5Bdata%5D=0&columns%5B0%5D%5Bname%5D=mvpage&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=1&columns%5B1%5D%5Bname%5D=rel_date&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=2&columns%5B2%5D%5Bname%5D=artist&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=3&columns%5B3%5D%5Bname%5D=song&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=4&columns%5B4%5D%5Bname%5D=hangul&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=5&columns%5B5%5D%5Bname%5D=director&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=6&columns%5B6%5D%5Bname%5D=videourl&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=7&columns%5B7%5D%5Bname%5D=groupt&columns%5B7%5D%5Bsearchable%5D=true&columns%5B7%5D%5Borderable%5D=true&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B8%5D%5Bdata%5D=8&columns%5B8%5D%5Bname%5D=reltype&columns%5B8%5D%5Bsearchable%5D=true&columns%5B8%5D%5Borderable%5D=true&columns%5B8%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B8%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=1&order%5B0%5D%5Bdir%5D=desc&start=0&length=-1&search%5Bvalue%5D=&search%5Bregex%5D=false&wdtNonce=706aa71f49';
+
+var options = {
+    url: 'https://dbkpop.com/wp-admin/admin-ajax.php?action=get_wdtable&table_id=9',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+var youtubeExtractRegex = /href="(.*)"/;
+
 db.serialize(() => {
-    db.run(`DROP TABLE IF EXISTS artists`);
     db.run(`DROP TABLE IF EXISTS videos`);
-    db.run(`CREATE TABLE artists (id INTEGER PRIMARY KEY, name TEXT, gender TEXT, formation_date TEXT, followers INTEGER)`);
-    db.run(`CREATE TABLE videos (id INTEGER PRIMARY KEY, artistID TEXT, name TEXT, video_type TEXT, youtube_link TEXT, views INTEGER, publish_date TEXT, dead TEXT, FOREIGN KEY(artistID) references artists(id))`);
+    db.run(`CREATE TABLE videos (id INTEGER PRIMARY KEY, date TEXT, artist TEXT, songName TEXT, koreanName TEXT, videoLink TEXT, type TEXT, release TEXT)`);
     db.parallelize(() => {
-        request('http://www.aoimirai.net/kpop/api.php?command=listVideos&listType=full', function (error, response, body) {
-            console.log("Importing video data");
+        request(options, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                let videos = JSON.parse(body);
-                for (let i = 0; i < videos.length; i++) {
-                    let video = videos[i];
-                    let insert_query = "INSERT INTO videos(artistID, name, video_type, youtube_link, views, publish_date, dead) VALUES(?, ?, ?, ?, ?, ?, ?)";
-                    db.run(insert_query, [video.artistID, video.musicName, video.videoType, video.link, video.views, video.publish_date, video.isDead], (err) => {
-                        if (err) {
+                let result = JSON.parse(body);
+                console.log("Records retrieved: " + result.recordsTotal);
+                for (let i = 0; i < result.data.length; i ++) {
+                    let musicVideo = result.data[i];
+                    let date = musicVideo[1];
+                    let artist = musicVideo[2];
+                    let songName = musicVideo[3];
+                    let koreanName = musicVideo[4];
+                    let videoLink = musicVideo[6].match(youtubeExtractRegex)[1];
+                    //Girl, Boy, Boy Solo, Girl Solo, Co-ed, Co-Ed
+                    let type = musicVideo[7];
+                    //Major, Japanese, Special, Minor, English, CF, OST, Chinese
+                    let release = musicVideo[8];
+                    let insertQuery = "INSERT INTO videos(id, date, artist, songName, koreanName, videoLink, type, release) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+                    db.run(insertQuery, [i, date, artist, songName, koreanName, videoLink, type, release], (e) => {
+                        if (e){
                             console.log("Failed inserting video: " + err);
                             return;
                         }
                     })
                 }
             }
-        })
-        request('http://www.aoimirai.net/kpop/api.php?command=listArtists&listType=full', function (error, response, body) {
-            console.log("Importing artist data");
-            if (!error && response.statusCode == 200) {
-                let artists = JSON.parse(body);
-                let insert_query = "INSERT INTO artists(id, name, gender, formation_date, followers) VALUES(?, ?, ?, ?, ?)";
-                for (let i = 0; i < artists.length; i++) {
-                    let artist = artists[i];
-                    db.run(insert_query, [artist.artistID, artist.artistName, artist.gender, artist.debutYear, artist.followersTotal], (err) => {
-                        if (err) {
-                            console.err("Failed inserting artist: " + err);
-                            return;
-                        }
-                    })
-                }
-            }
-        })
+        });
     })
 })
