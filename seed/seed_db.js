62419e93b363b4e5d2908788d7386ca6ac65913c
Brian Le <brainicism@gmail.com>
2020-02-19 23:02:09 -0500

    Programmatically DROP/CREATE database, refactor seed

--- seed/seed_db.js
+++ seed/seed_db.js
@@ -1,12 +1,14 @@
-var request = require('request-promise')
-var fs = require("fs")
-const mkdirp = require('mkdirp')
-const { execSync } = require('child_process');
-var unzipper = require("unzipper")
+const request = require("request-promise")
+const fs = require("fs")
+const mkdirp = require("mkdirp")
+const { execSync } = require("child_process");
+const unzipper = require("unzipper")
+const mysql = require("promise-mysql");
 
-var fileUrl = "http://kpop.aoimirai.net/download.php";
+const fileUrl = "http://kpop.aoimirai.net/download.php";
 
-//TODO: this file is kinda scuffed, refactor later
+
+//TODO: this is probably not how you use promises fix later
 
 let options = {
   url: fileUrl,
@@ -16,29 +18,57 @@ let options = {
     "User-Agent": "PostmanRuntime/7.22.0"
   }
 }
+const kmqTempDir = "/tmp/kmq";
+mkdirp(kmqTempDir)
+mkdirp(`${kmqTempDir}/sql`)
+const output = `${kmqTempDir}/bootstrap.zip`
 
-mkdirp("/tmp/kmq")
-mkdirp("/tmp/kmq/sql")
-let output = "/tmp/kmq/bootstrap.zip"
-request(options)
-  .then((resp, body) => {
-    fs.writeFile(output, resp, function (err) {
-      console.log("Downloaded database.zip");
-    });
-  })
-  .then(() => {
-    fs.createReadStream('/tmp/kmq/bootstrap.zip')
-      .pipe(unzipper.Extract({ path: '/tmp/kmq/sql/' }))
-      .on("error", (err) => {
-        console.log("Extracted database.zip");
-        // this throws an error even though it finished successfully
-        fs.readdir("/tmp/kmq/sql", (err, files) => {
-          execSync(`mysql -e " DROP DATABASE IF EXISTS kmq";`)
-          execSync(`mysql -e " CREATE DATABASE kmq";`)
-          execSync(`mysql kmq < /tmp/kmq/sql/${files[0]}`)
+let main = async function (){ 
+  db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+  });
+
+  request(options)
+    .then((resp, body) => {
+      return new Promise((resolve, reject) => {
+        fs.writeFile(output, resp, function (err) {
+          console.log("Downloaded database.zip");
+          resolve();
+        });
+      })
+    })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        fs.createReadStream(`${kmqTempDir}/bootstrap.zip`)
+          .pipe(unzipper.Extract({ path: `${kmqTempDir}/sql/` }))
+          .on("error", (err) => {
+            // this throws an error even though it finished successfully
+            if (!err.toString().includes("invalid signature")) {
+              reject(err);
+            }
+            console.log("Extracted database.zip");
+            resolve();
+          })
+          .on("finish", () => resolve())
+      })
+    })
+    .then(async () => {
+      return new Promise((resolve, reject) => {
+        fs.readdir(`${kmqTempDir}/sql`, async (err, files) => {
+          console.log("Dropping KMQ database");
+          await db.query("DROP DATABASE IF EXISTS kmq;");
+          console.log("Creating KMQ database")
+          await db.query("CREATE DATABASE kmq;");
+          console.log("Seeding KMQ database");
+          execSync(`mysql kmq < ${kmqTempDir}/sql/${files[0]}`)
           console.log(`Imported database dump (${files[0]}) successfully`);
+          //this is awful but idk why it won't end
+          process.exit();
         })
       })
-  })
-
-  .catch(e => console.log(e.toString()))
+    })
+    .catch(e => console.log(e))
+}
+main()
