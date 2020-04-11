30e5abe8270f323c905ace8f54fdc73d424a8044
taahamahdi <mahditaaha11@gmail.com>
2020-04-10 22:43:14 -0400

    Start new song when current one ends, increase playback bitrate [#81]

--- helpers/utils.js
+++ helpers/utils.js
@@ -3,24 +3,51 @@ const ytdl = require("ytdl-core");
 const fetchVideoInfo = require("youtube-info");
 const hangulRomanization = require('hangul-romanization');
 
-module.exports = {
-    sendSongMessage: (message, gameSession, isForfeit) => {
+const startGame = (gameSession, guildPreference, db, message) => {
+    if (gameSession.gameInSession()) {
         message.channel.send({
             embed: {
                 color: RED,
-                author: {
-                    name: isForfeit ? null : message.author.username,
-                    icon_url: isForfeit ? null : message.author.avatarURL
-                },
-                title: `"${gameSession.getSong()}" - ${gameSession.getArtist()}`,
-                description: `https://youtube.com/watch?v=${gameSession.getLink()}\n\n**Scoreboard**`,
-                image: {
-                    url: `https://img.youtube.com/vi/${gameSession.getLink()}/hqdefault.jpg`
-                },
-                fields: gameSession.scoreboard.getScoreboard()
+                title: `Game already in session`
             }
         })
-    },
+        return;
+    }
+    let query = `SELECT nome as name, name as artist, vlink as youtubeLink FROM app_kpop INNER JOIN app_kpop_group ON app_kpop.id_artist = app_kpop_group.id
+    WHERE FIND_IN_SET(members, ?) AND dead = "n" AND publishedon >= "?-01-01" AND vtype = "main"
+    ORDER BY app_kpop.views DESC LIMIT ?;`;
+    db.query(query, [guildPreference.getSQLGender(), guildPreference.getBeginningCutoffYear(), guildPreference.getLimit()])
+    .then((result) => {
+        let random = result[Math.floor(Math.random() * result.length)];
+        gameSession.startRound(random.name, random.artist, random.youtubeLink);
+        playSong(gameSession, guildPreference, db, message);
+    })
+    .catch((err) => {
+        console.log(err);
+        message.channel.send(err.toString());
+    })
+}
+const sendSongMessage = (message, gameSession, isForfeit) => {
+    message.channel.send({
+        embed: {
+            color: RED,
+            author: {
+                name: isForfeit ? null : message.author.username,
+                icon_url: isForfeit ? null : message.author.avatarURL
+            },
+            title: `"${gameSession.getSong()}" - ${gameSession.getArtist()}`,
+            description: `https://youtube.com/watch?v=${gameSession.getLink()}\n\n**Scoreboard**`,
+            image: {
+                url: `https://img.youtube.com/vi/${gameSession.getLink()}/hqdefault.jpg`
+            },
+            fields: gameSession.scoreboard.getScoreboard()
+        }
+    })
+}
+
+module.exports = {
+    startGame,
+    sendSongMessage,
     sendScoreboard: (message, gameSession) => {
         message.channel.send({
             embed: {
@@ -36,30 +63,6 @@ module.exports = {
             voiceConnection.disconnect();
             return;
         }
-    },
-    startGame: (gameSession, guildPreference, db, message) => {
-        if (gameSession.gameInSession()) {
-            message.channel.send({
-                embed: {
-                    color: RED,
-                    title: `Game already in session`
-                }
-            })
-            return;
-        }
-        let query = `SELECT nome as name, name as artist, vlink as youtubeLink FROM app_kpop INNER JOIN app_kpop_group ON app_kpop.id_artist = app_kpop_group.id
-        WHERE FIND_IN_SET(members, ?) AND dead = "n" AND publishedon >= "?-01-01" AND vtype = "main"
-        ORDER BY app_kpop.views DESC LIMIT ?;`;
-        db.query(query, [guildPreference.getSQLGender(), guildPreference.getBeginningCutoffYear(), guildPreference.getLimit()])
-        .then((result) => {
-            let random = result[Math.floor(Math.random() * result.length)];
-            gameSession.startRound(random.name, random.artist, random.youtubeLink);
-            playSong(gameSession, guildPreference, db, message);
-        })
-        .catch((err) => {
-            console.log(err);
-            message.channel.send(err.toString());
-        })
     },
     getUserIdentifier: (user) => {
         return `${user.username}#${user.discriminator}`
@@ -85,16 +88,23 @@ module.exports = {
         return message.member.voiceChannel.members.size - 1;
     }
 }
+
 const playSong = (gameSession, guildPreference, db, message) => {
     let voiceChannel = message.member.voiceChannel;
-    const streamOptions = { volume: 0.1 };
+    const streamOptions = { volume: 0.1, bitrate: 192000 };
     voiceChannel.join().then(connection => {
         let options = { filter: "audioonly", quality: "highest" };
         const stream = ytdl(gameSession.getLink(), options);
         const dispatcher = connection.playStream(stream, streamOptions);
+        stream.on('end', () => {
+            sendSongMessage(message, gameSession, true);
+            gameSession.endRound();
+            startGame(gameSession, guildPreference, db, message);
+        })
     }).catch((err) => {
         console.error(err);
         // Attempt to restart game with different song
+        sendSongMessage(message, gameSession, true);
         gameSession.endRound();
         startGame(gameSession, guildPreference, db, message);
     })
