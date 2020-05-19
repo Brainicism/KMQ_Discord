43f33af3784caf6f20375f564befba6ad09ed9c4
taahamahdi <mahditaaha11@gmail.com>
2020-05-19 13:30:32 -0400

    Corrected path to config for seed, updated npm run seed

--- seed/seed_db.js
+++ seed/seed_db.js
@@ -4,10 +4,10 @@ const mkdirp = require("mkdirp");
 const { execSync } = require("child_process");
 const unzipper = require("unzipper")
 const mysql = require("promise-mysql");
-const config = require("../app_config.json");
+const config = require("../config/app_config.json");
 const rmfr = require('rmfr');
 const fileUrl = "http://kpop.aoimirai.net/download.php";
-const logger = require("../logger")("seed_db");
+const logger = require("../src/logger")("seed_db");
 const prependFile = require('prepend-file');
 
 //TODO: this is probably not how you use promises fix later
