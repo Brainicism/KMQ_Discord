bfd0fbc17aa730ec16cec78c0f58cae4fd1f2abf
Brian Le <brainicism@gmail.com>
2020-05-22 16:32:56 -0400

    Save database downloads permanently instead of tmp [#84]

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,5 @@
 import * as log4js from "log4js";
 export default (name: string): log4js.Logger => {
-    log4js.configure("../config/log_config.json");
+    log4js.configure(__dirname + "/../config/log_config.json");
     return log4js.getLogger(name);
 }
