4a78e822c06d6a307913361add6b0cd2e2c712b9
Brian Le <brainicism@gmail.com>
2020-08-02 22:41:31 -0400

    Added games played and songs guessed server statistics

--- migrations/20200802155423_usage-data.js
+++ migrations/20200802155423_usage-data.js
@@ -3,6 +3,8 @@ exports.up = function (knex) {
     return knex.schema.table('guild_preferences', function (table) {
         table.datetime('join_date');
         table.datetime('last_active');
+        table.integer('games_played').defaultTo(0);
+        table.integer('songs_guessed').defaultTo(0);
     });
 };
 
@@ -10,5 +12,7 @@ exports.down = function (knex) {
     return knex.schema.table('guild_preferences', function (table) {
         table.dropColumn('join_date');
         table.dropColumn('last_active');
+        table.dropColumn('games_played');
+        table.dropColumn('songs_guessed');
     });
 };
