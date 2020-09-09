3d7d4201388f2949a0c40da2a83f5d64904c9612
Brian Le <brainicism@gmail.com>
2020-09-09 01:38:08 +0000

    Add weird apostrophe as exception to get-unclean-song-names

--- src/scripts/get-unclean-song-names.ts
+++ src/scripts/get-unclean-song-names.ts
@@ -14,7 +14,7 @@ import existingSongAliases from "../../data/song_aliases.json";
     const query = `SELECT nome as name, name as artist, vlink as youtubeLink FROM kpop_videos.app_kpop INNER JOIN kpop_videos.app_kpop_group ON kpop_videos.app_kpop.id_artist = kpop_videos.app_kpop_group.id
     WHERE dead = "n" AND vtype = "main";`;
     const songs: Array<QueriedSong> = await db.query(query);
-    const nonAsciiSongs = songs.filter(x => !/^[\x00-\x7F]*$/.test(x.name));
+    const nonAsciiSongs = songs.filter(x => !/^[\x00-\x7F’]*$/.test(x.name));
     const nonCheckedSongs = nonAsciiSongs.filter(x => !(x.youtubeLink in existingSongAliases));
     fs.writeFileSync("./tmp/song_dump.txt", nonCheckedSongs.map((x => `${x.youtubeLink}, ${x.name}`)).join("\n"));
     console.log("Done");
