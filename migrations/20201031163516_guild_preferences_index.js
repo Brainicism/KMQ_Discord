6a221eb3c2cd5920a0d1afc8a25110170cb34b6c
Brian Le <brainicism@gmail.com>
2020-10-31 17:09:31 -0400

    Add migration for guild preference index

---
+++ migrations/20201031163516_guild_preferences_index.js
@@ -0,0 +1,10 @@
+
+exports.up = function (knex) {
+    return knex.raw("ALTER TABLE guild_preferences ADD INDEX guild_preferences_id_idx(guild_id(20));")
+};
+
+exports.down = function (knex) {
+    return knex.schema.table("guild_preferences", function (table) {
+        table.dropIndex(["guild_id"], "guild_preferences_id_idx");
+    });
+};
