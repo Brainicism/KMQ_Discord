e3a35ddad1137b0381f2c75beb5ac5dfac06c930
Brian Le <brainicism@gmail.com>
2020-11-01 23:00:51 +0000

    Fix logger.ts ESLint violations

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,9 @@
+/* eslint-disable quote-props */
 import log4js from "log4js";
 import { config } from "dotenv";
 import { resolve } from "path";
-config({ path: resolve(__dirname, "../../.env") })
+
+config({ path: resolve(__dirname, "../../.env") });
 
 export default (name: string): log4js.Logger => {
     log4js.configure({
@@ -10,13 +12,12 @@ export default (name: string): log4js.Logger => {
             "error": { "type": "dateFile", "filename": `${process.env.LOG_DIR}/error.log` },
             "error-filtered": { "type": "logLevelFilter", "appender": "error", "level": "error" },
             "info-filtered": { "type": "logLevelFilter", "appender": "log", "level": "info" },
-            "console": { "type": "console" }
+            "console": { "type": "console" },
         },
         "categories": {
             "default": { "appenders": ["error-filtered", "console", "info-filtered"], "level": "info" },
-            "debug": { "appenders": ["error"], "level": "debug" }
-        }
-    }
-    );
+            "debug": { "appenders": ["error"], "level": "debug" },
+        },
+    });
     return log4js.getLogger(name);
 };
