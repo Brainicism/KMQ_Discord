f7cdfd48a139914c7ef79d10350d9f3e7800f3ce
Brian Le <brainicism@gmail.com>
2020-12-29 17:37:35 -0500

    Add tsdocs for remaining files in src/model

--- src/models/scoreboard.ts
+++ src/models/scoreboard.ts
@@ -2,10 +2,13 @@ import { roundDecimal } from "../helpers/utils";
 import Player from "./player";
 
 export default class Scoreboard {
+    /** Mapping of Discord user ID to Player */
     protected players: { [userID: number]: Player };
 
+    /** The current players in first place */
     protected firstPlace: Array<Player>;
 
+    /** The current highest score */
     private highestScore: number;
 
     constructor() {
@@ -14,15 +17,16 @@ export default class Scoreboard {
         this.highestScore = 0;
     }
 
+    /** @returns a string congratulating the winner(s) */
     getWinnerMessage(): string {
         let winnerStr = "";
 
         if (this.firstPlace.length === 1) {
-            return `${this.firstPlace[0].getName()} wins!`;
+            return `${this.firstPlace[0].getTag()} wins!`;
         }
 
         for (let i = 0; i < this.firstPlace.length; i++) {
-            winnerStr += this.firstPlace[i].getName();
+            winnerStr += this.firstPlace[i].getTag();
             if (i === this.firstPlace.length - 1) {
                 // Last entry -- append just the username
                 winnerStr += " ";
@@ -38,17 +42,19 @@ export default class Scoreboard {
         return winnerStr;
     }
 
+    /** @returns An array of DiscordEmbed fields representing each participant's lives */
     getScoreboardEmbedFields(): Array<{ name: string, value: string, inline: boolean }> {
         return Object.values(this.players)
             .sort((a, b) => b.getScore() - a.getScore())
             .map((x) => (
                 {
-                    name: x.getName(),
+                    name: x.getTag(),
                     value: Number.isInteger(roundDecimal(x.getScore(), 1)) ? roundDecimal(x.getScore(), 1).toString() : x.getScore().toFixed(1),
                     inline: true,
                 }));
     }
 
+    /** @returns a list containing each player ID and their corresponding score */
     getPlayerScores(): Array<{ id: string, score: number }> {
         return Object.values(this.players)
             .map((x) => ({
@@ -57,6 +63,12 @@ export default class Scoreboard {
             }));
     }
 
+    /**
+     * @param winnerTag - The Discord tag of the correct guesser
+     * @param winnerID  - The Discord ID of the correct guesser
+     * @param avatarURL - The avatar URL of the correct guesser
+     * @param pointsEarned - The amount of points awarded
+     */
     updateScoreboard(winnerTag: string, winnerID: string, avatarURL: string, pointsEarned: number) {
         if (!this.players[winnerID]) {
             this.players[winnerID] = new Player(winnerTag, winnerID, avatarURL, pointsEarned);
@@ -74,14 +86,20 @@ export default class Scoreboard {
         }
     }
 
+    /** @returns whether the scoreboard has any players on it */
     isEmpty(): boolean {
         return !(Object.keys(this.players).length);
     }
 
+    /** @returns a list of the player currently in first place */
     getWinners(): Array<Player> {
         return this.firstPlace;
     }
 
+    /**
+     * @param userId - The Discord user ID of the player to check
+     * @returns the score of the player
+     */
     getPlayerScore(userId: string): number {
         if (userId in this.players) {
             return this.players[userId].getScore();
@@ -89,11 +107,13 @@ export default class Scoreboard {
         return 0;
     }
 
+    /** @returns whether the game has completed */
     gameFinished(goal: number): boolean {
         return this.firstPlace[0].getScore() >= goal;
     }
 
+    /** @returns a list of tags of the player participating in the game */
     getPlayerNames(): Array<string> {
-        return Object.values(this.players).map((player) => player.getName());
+        return Object.values(this.players).map((player) => player.getTag());
     }
 }
