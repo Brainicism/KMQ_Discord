6107801d0d4a2d1b71b57af93d7223173af4b3dc
Brian Le <brainicism@gmail.com>
2020-12-31 21:29:40 -0500

    Add GAON weekly chart facts

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -94,3 +94,18 @@ export function arrayToString(arr: Array<string>): string {
     const lastElement = elements.splice(-1);
     return `${elements.join(", ")} and ${lastElement}`;
 }
+
+/**
+ * Stolen from https://weeknumber.net/how-to/javascript
+ * @returns the current ISO week number in the year, ranges from 1-52 (53 on certain years)
+ */
+export function weekOfYear(dateObj?: Date): number {
+    const date = dateObj || new Date();
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+    // January 4 is always in week 1.
+    const week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
+}
