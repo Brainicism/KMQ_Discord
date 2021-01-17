e7b06649e9b3204b00e23055e72c85dd41ddf712
Brian Le <brainicism@gmail.com>
2021-01-17 15:12:25 -0500

    Add leaderboard pagination

--- src/commands/game_commands/leaderboard.ts
+++ src/commands/game_commands/leaderboard.ts
@@ -4,14 +4,20 @@ import BaseCommand, { CommandArgs } from "../base_command";
 import _logger from "../../logger";
 import { getDebugLogHeader, getUserTag, sendEmbed, sendErrorMessage, sendInfoMessage } from "../../helpers/discord_utils";
 import { getRankNameByLevel } from "./profile";
-import { bold } from "../../helpers/utils";
+import { bold, chooseRandom } from "../../helpers/utils";
 
 const logger = _logger("leaderboard");
 
 enum LeaderboardAction {
     ENROLL = "enroll",
     UNEROLL = "unenroll",
+    PAGE = "page",
 }
+
+const leaderboardQuotes = [
+    `Want your name to show up on the leaderboard? See \`${process.env.BOT_PREFIX}help leaderboard\``,
+    `Want to see the next page of the leaderboard? See \`${process.env.BOT_PREFIX}help leaderboard\``,
+];
 
 export default class LeaderboardCommand implements BaseCommand {
     help = {
@@ -25,11 +31,15 @@ export default class LeaderboardCommand implements BaseCommand {
             },
             {
                 example: "`!leaderboard enroll`",
-                explanation: "Displays your name on the leaderboard if you are in the top 10",
+                explanation: "Allows your name to be displayed on the leaderboard",
             },
             {
                 example: "`!leaderboard unenroll`",
-                explanation: "Hides your name from the leaderboard if you are in the top 10",
+                explanation: "Hides your name from the leaderboard",
+            },
+            {
+                example: "`!leaderboard page 3`",
+                explanation: "Shows the 3rd page of the leaderboard",
             },
         ],
         priority: 50,
@@ -37,25 +47,45 @@ export default class LeaderboardCommand implements BaseCommand {
 
     validations = {
         minArgCount: 0,
-        maxArgCount: 1,
+        maxArgCount: 2,
         arguments: [
             {
                 name: "action",
                 type: "enum" as const,
                 enums: Object.values(LeaderboardAction),
             },
+            {
+                name: "page_number",
+                type: "number" as const,
+                minValue: 1,
+            },
         ],
     };
 
+    aliases = ["lb"];
+
     async call({ message, parsedMessage }: CommandArgs) {
         if (parsedMessage.components.length === 0) {
-            this.showLeaderboard(message);
-        } else {
-            const action = parsedMessage.components[0] as LeaderboardAction;
+            this.showLeaderboard(message, 0);
+            return;
+        }
+        const action = parsedMessage.components[0] as LeaderboardAction;
+        if (parsedMessage.components.length === 1) {
             if (action === LeaderboardAction.ENROLL) {
                 this.enrollLeaderboard(message);
-            } else {
+            } else if (action === LeaderboardAction.UNEROLL) {
                 this.unenrollLeaderboard(message);
+            } else if (action === LeaderboardAction.PAGE) {
+                this.showLeaderboard(message, 0);
+            }
+            return;
+        }
+
+        if (parsedMessage.components.length === 2) {
+            if (action === LeaderboardAction.PAGE) {
+                this.showLeaderboard(message, 10 * (parseInt(parsedMessage.components[1], 10) - 1));
+            } else {
+                sendErrorMessage(message, "Incorrect Leaderboard Usage", `See \`${process.env.BOT_PREFIX}help leaderboard\` for more details`);
             }
         }
     }
@@ -75,7 +105,7 @@ export default class LeaderboardCommand implements BaseCommand {
                 player_id: message.author.id,
                 display_name: getUserTag(message.author),
             });
-        sendInfoMessage(message, "Leaderboard Enrollment Complete", "You are now visible on the leaderboard if you are in the Top 10");
+        sendInfoMessage(message, "Leaderboard Enrollment Complete", "Your name is now visible on the leaderboard");
     }
 
     private async unenrollLeaderboard(message: Eris.Message<GuildTextableChannel>) {
@@ -84,19 +114,25 @@ export default class LeaderboardCommand implements BaseCommand {
             .del();
         sendInfoMessage(message, "Leaderboard Unenrollment Complete", "You are no longer visible on the leaderboard");
     }
-    private async showLeaderboard(message: Eris.Message<GuildTextableChannel>) {
+    private async showLeaderboard(message: Eris.Message<GuildTextableChannel>, offset: number) {
         const topPlayers = await dbContext.kmq("player_stats")
             .select(["exp", "level", "player_id"])
             .orderBy("exp", "DESC")
+            .where("exp", ">", 0)
+            .offset(offset)
             .limit(10);
 
+        if (topPlayers.length === 0) {
+            sendErrorMessage(message, "😐", "The leaderboard doesn't go this far");
+            return;
+        }
         logger.info(`${getDebugLogHeader(message)} | Leaderboard retrieved`);
         const fields: Array<Eris.EmbedField> = await Promise.all(topPlayers.map(async (player, rank) => {
             const enrolledPlayer = await dbContext.kmq("leaderboard_enrollment")
                 .where("player_id", "=", player.player_id)
                 .first();
             return {
-                name: enrolledPlayer ? enrolledPlayer.display_name : `Rank #${rank + 1}`,
+                name: enrolledPlayer ? enrolledPlayer.display_name : `Rank #${(rank + offset) + 1}`,
                 value: `${player.exp} xp | Level ${player.level} (${getRankNameByLevel(player.level)})`,
             };
         }));
@@ -106,7 +142,7 @@ export default class LeaderboardCommand implements BaseCommand {
             fields,
             timestamp: new Date(),
             footer: {
-                text: `On this list and want your name to show up? See \`${process.env.BOT_PREFIX}help leaderboard\``,
+                text: chooseRandom(leaderboardQuotes),
             },
         });
     }
