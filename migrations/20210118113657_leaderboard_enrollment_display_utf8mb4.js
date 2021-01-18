cef96d8daf0b6b49240ac95a608eb427b209ed3b
Brian Le <brainicism@gmail.com>
2021-01-18 11:39:15 -0500

    Use utf8mb4 encoding for display names in leaderboard enrollment

---
+++ migrations/20210118113657_leaderboard_enrollment_display_utf8mb4.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+    return knex.schema.alterTable("leaderboard_enrollment", (table) => {
+        table.string("display_name")
+        .collate('utf8mb4_general_ci')
+        .notNullable()
+        .alter();
+    })
+};
+
+exports.down = function(knex) {
+};
