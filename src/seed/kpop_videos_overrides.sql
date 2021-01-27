3df6f1da8b3ab618d143472057421fc7e47e7033
Brian Le <brainicism@gmail.com>
2021-01-26 21:58:39 -0500

    Add data overrides file

---
+++ src/seed/kpop_videos_overrides.sql
@@ -0,0 +1,2 @@
+UPDATE kpop_videos.app_kpop SET nome = 'Roly Poly in Copabana' WHERE vlink = '3Xolk2cFzlo';
+UPDATE kpop_videos.app_kpop SET nome = 'Roly Poly', vtype = 'main' WHERE vlink = 'afwK0Mv0IsY';
