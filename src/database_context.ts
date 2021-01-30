52080b59006c6241dc39e27525d79c45c490ed64
Brian Le <brainicism@gmail.com>
2021-01-30 17:22:29 -0500

    Remove dependency on mysql-promise (#507)

--- src/database_context.ts
+++ src/database_context.ts
@@ -1,26 +1,38 @@
 import Knex from "knex";
 import kmqKnexConfig from "./config/knexfile_kmq";
 import kpopVideosKnexConfig from "./config/knexfile_kpop_videos";
+import agnosticKnexConfig from "./config/knexfile_agnostic";
 import _logger from "./logger";
 import { EnvType } from "./types";
 
 const logger = _logger("database_context");
 
-class DatabaseContext {
+export class DatabaseContext {
     public kmq: Knex;
     public kpopVideos: Knex;
+    public agnostic: Knex;
 
-    constructor() {
+    constructor(initAgnostic = false) {
         if (process.env.NODE_ENV === EnvType.DRY_RUN) return;
         logger.info("Initializing database connections");
         this.kmq = Knex(kmqKnexConfig);
         this.kpopVideos = Knex(kpopVideosKnexConfig);
+        if (initAgnostic) {
+            this.agnostic = Knex(agnosticKnexConfig);
+        }
     }
 
     async destroy() {
         await this.kmq.destroy();
         await this.kpopVideos.destroy();
+        if (this.agnostic) {
+            await this.agnostic.destroy();
+        }
     }
+}
+
+export function getDatabaseAgnosticContext(): DatabaseContext {
+    return new DatabaseContext(true);
 }
 
 export default new DatabaseContext();
