d728539f9e1d6ee9debc0ee01e1e0ebc9e2a3937
Brian Le <brainicism@gmail.com>
2021-01-30 18:03:56 +0000

    Update get-unclean-song-names script

--- src/scripts/get-unclean-song-names.ts
+++ src/scripts/get-unclean-song-names.ts
@@ -1,28 +1,26 @@
-import mysql from "promise-mysql";
-import fs from "fs";
 import path from "path";
 import { config } from "dotenv";
 import { QueriedSong } from "../types";
 import { parseJsonFile } from "../helpers/utils";
+import dbContext from "../database_context";
+import _logger from "../logger";
 
+const logger = _logger("get-unclean-song-names");
 const existingSongAliases = parseJsonFile(path.resolve(__dirname, "../../data/song_aliases.json"));
 config({ path: path.resolve(__dirname, "../../.env") });
 
 (async () => {
-    const db = await mysql.createPool({
-        connectionLimit: 10,
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-    });
-    const query = `SELECT nome as name, name as artist, vlink as youtubeLink FROM kpop_videos.app_kpop INNER JOIN kpop_videos.app_kpop_group ON kpop_videos.app_kpop.id_artist = kpop_videos.app_kpop_group.id
-    WHERE dead = "n" AND vtype = "main";`;
-    const songs: Array<QueriedSong> = await db.query(query);
+    const songs: Array<QueriedSong> = await dbContext.kmq("available_songs")
+        .select(["song_name as name", "artist_name as artist", "link as youtubeLink"]);
     // eslint-disable-next-line no-control-regex
-    const nonAsciiSongs = songs.filter((x) => !/^[\x00-\x7F’]*$/.test(x.name));
+    const nonAsciiSongs = songs.filter((x) => !/^[\x00-\x7F’]*$/.test(x.name.split("(")[0].trim()));
     const nonCheckedSongs = nonAsciiSongs.filter((x) => !(x.youtubeLink in existingSongAliases));
-    fs.writeFileSync("/tmp/song_dump.txt", nonCheckedSongs.map(((x) => `${x.youtubeLink}, ${x.name}`)).join("\n"));
     // eslint-disable-next-line no-console
-    console.log("Done");
-    await db.end();
+    if (nonCheckedSongs.length) {
+        logger.info(nonCheckedSongs.map(((x) => `${x.youtubeLink}, ${x.name}`)).join("\n"));
+    } else {
+        logger.info("Nothing found");
+    }
+
+    await dbContext.destroy();
 })();
