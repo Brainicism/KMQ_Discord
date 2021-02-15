c6a88b92551bcba8d1bf7b5a119ba68833980fa8
taahamahdi <mahditaaha11@gmail.com>
2021-02-15 14:49:46 -0500

    Added option to import sql dump with --import <file>

--- src/scripts/backup-kmq-database.ts
+++ src/scripts/backup-kmq-database.ts
@@ -1,8 +1,18 @@
 import fs from "fs";
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
+import { Logger } from "log4js";
+import _logger from "../logger";
+import { program } from "commander";
 import { friendlyFormattedDate } from "../helpers/utils";
 
 const databaseBackupDir = `${process.env.AOIMIRAI_DUMP_DIR}/kmq_backup`;
+const logger: Logger = _logger("backup-kmq");
+
+program
+    .option("-i, --import <file>", "The dump file to import")
+
+program.parse();
+const options = program.opts();
 
 async function backupKmqDatabase() {
     if (!fs.existsSync(databaseBackupDir)) {
@@ -11,9 +21,21 @@ async function backupKmqDatabase() {
     exec(`mysqldump -u ${process.env.DB_USER} -p${process.env.DB_PASS} -h ${process.env.DB_HOST} --routines kmq > ${databaseBackupDir}/kmq_backup_${friendlyFormattedDate(new Date())}.sql`);
 }
 
+function importKmqDatabase(fileWithPath: string) {
+    if (!fs.existsSync(fileWithPath)) {
+        logger.error(`Dump file ${fileWithPath} doesn't exist.`)
+        return;
+    }
+    execSync(`mysqldump -u ${process.env.DB_USER} -p${process.env.DB_PASS} -h ${process.env.DB_HOST} kmq < ${fileWithPath}`);
+}
+
 (async () => {
     if (require.main === module) {
-        backupKmqDatabase();
+        if (options.import) {
+            importKmqDatabase(options.import);
+        } else {
+            backupKmqDatabase();
+        }
     }
 })();
 
