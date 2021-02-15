613136a4e04a16a10954a4b80c9bbe27b58c0b1f
Taaha <mahditaaha11@gmail.com>
2021-02-15 17:14:00 -0500

    Add script run every Sunday to backup kmq db (#552)
    * Add script run every Sunday to backup kmq db
    
    * Code review changes
    
    Made backupKmqDatabase async, added main method
    Corrected backup time
    Run backup at 1am instead of 12am
    
    * Added option to import sql dump with --import <file>
    
    * backupKmqDatabase now returns a Promise, is awaited
    
    * Add `npm run import -- <dump.sql>`
    
    * Use mysql instead of mysqldump for import

--- src/helpers/management_utils.ts
+++ src/helpers/management_utils.ts
@@ -30,6 +30,7 @@ import BotStatsPoster from "./bot_stats_poster";
 import { EnvType } from "../types";
 import storeDailyStats from "../scripts/store-daily-stats";
 import { seedAndDownloadNewSongs } from "../seed/seed_db";
+import backupKmqDatabase from "../scripts/backup-kmq-database";
 import { parseJsonFile } from "./utils";
 import { reloadFactCache } from "../fact_generator";
 
@@ -169,6 +170,7 @@ export function registerIntervals() {
         }
     });
 
+    // everyday at 12am UTC => 7pm EST
     schedule.scheduleJob("0 0 * * *", async () => {
         const serverCount = state.client.guilds.size;
         storeDailyStats(serverCount);
@@ -181,6 +183,13 @@ export function registerIntervals() {
         await seedAndDownloadNewSongs();
     });
 
+    // every sunday at 1am UTC => 8pm saturday EST
+    schedule.scheduleJob("0 1 * * 0", async () => {
+        logger.info("Backing up kmq database");
+        await backupKmqDatabase();
+    });
+
+    // every 5 minutes
     schedule.scheduleJob("*/5 * * * *", async () => {
         reloadAliases();
         updatePublishDateOverrides();
