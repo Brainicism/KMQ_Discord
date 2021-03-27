238ae7d73590e0937f4dcccb846cd3b155a242a5
Brian Le <brainicism@gmail.com>
2021-03-26 22:03:54 -0400

    Add tests for getMatchingGroupNames (#593)

--- src/helpers/game_utils.ts
+++ src/helpers/game_utils.ts
@@ -16,8 +16,8 @@ const GAME_SESSION_INACTIVE_THRESHOLD = 30;
 const logger = _logger("game_utils");
 
 interface GroupMatchResults {
-    unmatchedGroups?: Array<string>;
-    matchedGroups?: { id: number, name: string }[];
+    unmatchedGroups: Array<string>;
+    matchedGroups: { id: number, name: string }[];
 }
 
 /**
@@ -228,5 +228,6 @@ export async function getMatchingGroupNames(rawGroupNames: Array<string>): Promi
     }
     return {
         matchedGroups: matchingGroups,
+        unmatchedGroups: [],
     };
 }
