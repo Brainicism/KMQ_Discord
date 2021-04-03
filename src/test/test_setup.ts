51550d7bd33d84b1ab537fae6f907f754d5c3067
Brian Le <brainicism@gmail.com>
2021-04-03 15:01:01 -0400

    Perform migrations during test setup

--- src/test/test_setup.ts
+++ src/test/test_setup.ts
@@ -2,13 +2,18 @@
 import sinon from "sinon";
 import log4js from "log4js";
 import * as discordUtils from "../helpers/discord_utils";
+import kmqTestKnexConfig from "../config/knexfile_kmq_test";
+import dbContext from "../database_context";
 
 const sandbox = sinon.createSandbox();
 
-before(() => {
+before(async () => {
     sandbox.stub(discordUtils, "sendErrorMessage");
     sandbox.stub(discordUtils, "sendInfoMessage");
     log4js.getLogger().level = "off";
+    await dbContext.kmq.migrate.latest({
+        directory: kmqTestKnexConfig.migrations.directory,
+    });
     return false;
 });
 
