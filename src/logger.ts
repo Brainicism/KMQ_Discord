13521d09f7268cf0da7fc812572410eeeee7357e
Taaha Mahdi <mahditaaha11@gmail.com>
2021-04-05 17:15:06 -0400

    Removed LOG_DIR and DAISUKI_DUMP_DIR environment vars

--- src/logger.ts
+++ src/logger.ts
@@ -2,13 +2,19 @@
 import log4js from "log4js";
 import { config } from "dotenv";
 import { resolve } from "path";
+import fs from "fs";
 
 config({ path: resolve(__dirname, "../.env") });
 export default (name: string): log4js.Logger => {
+    const LOG_DIR = resolve(__dirname, "../logs");
+    if (!fs.existsSync(LOG_DIR)) {
+        fs.mkdirSync(LOG_DIR);
+    }
+
     log4js.configure({
         "appenders": {
-            "log": { "type": "dateFile", "filename": `${process.env.LOG_DIR}/log.log`, "daysToKeep": 10 },
-            "error": { "type": "dateFile", "filename": `${process.env.LOG_DIR}/error.log`, "daysToKeep": 10 },
+            "log": { "type": "dateFile", "filename": `${LOG_DIR}/log.log`, "daysToKeep": 10 },
+            "error": { "type": "dateFile", "filename": `${LOG_DIR}/error.log`, "daysToKeep": 10 },
             "error-filtered": { "type": "logLevelFilter", "appender": "error", "level": "error" },
             "info-filtered": { "type": "logLevelFilter", "appender": "log", "level": "info" },
             "console": { "type": "console" },
