501e40f74017e9773a551ba567d7ad3d87239445
Brian Le <brainicism@gmail.com>
2021-04-24 13:02:50 -0400

    Fix incorrect points calculation for ,mode both + ,multiguess on (#703)

--- src/structures/kmq_member.ts
+++ src/structures/kmq_member.ts
@@ -14,14 +14,18 @@ export default class KmqMember {
     /** The Discord ID */
     public id: string;
 
-    constructor(username: string, tag: string, avatarUrl: string, id: string) {
+    /** Number of points the member has received */
+    public pointsAwarded: number;
+
+    constructor(username: string, tag: string, avatarUrl: string, id: string, pointsAwarded = 0) {
         this.username = username;
         this.tag = tag;
         this.avatarUrl = avatarUrl;
         this.id = id;
+        this.pointsAwarded = pointsAwarded;
     }
 
-    static fromUser(user: Eris.User) {
-        return new KmqMember(user.username, getUserTag(user), user.avatarURL, user.id);
+    static fromUser(user: Eris.User, pointsAwarded = 0) {
+        return new KmqMember(user.username, getUserTag(user), user.avatarURL, user.id, pointsAwarded);
     }
 }
