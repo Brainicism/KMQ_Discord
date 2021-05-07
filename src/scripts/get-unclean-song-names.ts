03fa97bd97ea8af22239a3fc00fa4312750d5d91
Brian Le <brainicism@gmail.com>
2021-05-07 19:05:41 -0400

    Remove reference to song_aliases (#738)

--- src/scripts/get-unclean-song-names.ts
+++ src/scripts/get-unclean-song-names.ts
@@ -1,21 +1,16 @@
-import path from "path";
 import { QueriedSong } from "../types";
-import { parseJsonFile } from "../helpers/utils";
 import dbContext from "../database_context";
 import _logger from "../logger";
 
 const logger = _logger("get-unclean-song-names");
-const existingSongAliases = parseJsonFile(path.resolve(__dirname, "../../data/song_aliases.json"));
 
 (async () => {
     const songs: Array<QueriedSong> = await dbContext.kmq("available_songs")
         .select(["song_name as name", "artist_name as artist", "link as youtubeLink"]);
     // eslint-disable-next-line no-control-regex
     const nonAsciiSongs = songs.filter((x) => !/^[\x00-\x7F’]*$/.test(x.name.split("(")[0].trim()));
-    const nonCheckedSongs = nonAsciiSongs.filter((x) => !(x.youtubeLink in existingSongAliases));
-    // eslint-disable-next-line no-console
-    if (nonCheckedSongs.length) {
-        logger.info(nonCheckedSongs.map(((x) => `${x.youtubeLink}, ${x.name}`)).join("\n"));
+    if (nonAsciiSongs.length) {
+        logger.info(nonAsciiSongs.map(((x) => `${x.youtubeLink}, ${x.name}`)).join("\n"));
     } else {
         logger.info("Nothing found");
     }
