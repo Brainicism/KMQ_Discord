396c9334fb98fb8e97edba58533eb7381400866b
Brian Le <brainicism@gmail.com>
2021-05-10 01:51:54 -0400

    Include bootstrap upon dry-run

--- start.sh
+++ start.sh
@@ -5,21 +5,16 @@ rebuild () {
     echo "Compiling typescript..."
     tsc
 }
+rebuild
+echo "Bootstrapping..."
+node build/seed/bootstrap.js
+echo "Starting bot..."
+cd build/
 if [ "${NODE_ENV}" == "dry-run" ]; then
-    rebuild
-    echo "Starting bot..."
-    cd build/
     exec node kmq.js
-else
-    rebuild
-    echo "Bootstrapping..."
-    node build/seed/bootstrap.js
-    echo "Starting bot..."
-    cd build/
-    if [ "${NODE_ENV}" == "development" ]; then
-        exec node --inspect=9229 kmq.js
-    elif [ "${NODE_ENV}" == "production" ]; then
-        git log -n 1 --pretty=format:"%H" > ../version
-        exec node kmq.js
-    fi
+elif [ "${NODE_ENV}" == "development" ]; then
+    exec node --inspect=9229 kmq.js
+elif [ "${NODE_ENV}" == "production" ]; then
+    git log -n 1 --pretty=format:"%H" > ../version
+    exec node kmq.js
 fi
