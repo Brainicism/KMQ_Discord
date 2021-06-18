ce14d64bc23d5bb21ee5d847670e911109deeb19
taahamahdi <mahditaaha11@gmail.com>
2021-06-18 01:02:22 -0400

    Set DEFAULT_AUDIO_ONLY_SONGS_PER_ARTIST to 10

--- src/seed/seed_db.ts
+++ src/seed/seed_db.ts
@@ -15,7 +15,7 @@ config({ path: path.resolve(__dirname, "../../.env") });
 const SQL_DUMP_EXPIRY = 10;
 const mvFileUrl = "http://kpop.daisuki.com.br/download.php?file=full";
 const audioFileUrl = "http://kpop.daisuki.com.br/download.php?file=audio";
-const DEFAULT_AUDIO_ONLY_SONGS_PER_ARTIST = 25;
+const DEFAULT_AUDIO_ONLY_SONGS_PER_ARTIST = 10;
 const logger: Logger = _logger("seed_db");
 const databaseDownloadDir = path.join(__dirname, "../../sql_dumps/daisuki");
 if (!fs.existsSync(databaseDownloadDir)) {
