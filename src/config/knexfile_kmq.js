d7a4826f7472f3716580d20aed5836ebb40668d4
Brian Le <brainicism@gmail.com>
2021-06-20 16:51:04 -0400

    Apply workaround for mysql2 casting BIGINT to string (#820)

--- src/config/knexfile_kmq.js
+++ src/config/knexfile_kmq.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: resolve(__dirname, "../../.env")});
 module.exports = {
   client: 'mysql2',
   connection: {
-    user: process.env.DB_USER, password: process.env.DB_PASS, database: "kmq", host: process.env.DB_HOST, charset: "utf8mb4", port: parseInt(process.env.DB_PORT), multipleStatements: true
+    user: process.env.DB_USER, password: process.env.DB_PASS, database: "kmq", host: process.env.DB_HOST, charset: "utf8mb4", port: parseInt(process.env.DB_PORT), decimalNumbers: true
   },
   pool: {
     min: 0,
