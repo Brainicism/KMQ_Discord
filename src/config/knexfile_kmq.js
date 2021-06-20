5344bbe5f77dc8ef0855030f88806217dda5fff2
Brian Le <brainicism@gmail.com>
2021-06-20 00:43:30 -0400

    Switch to mysql2 driver

--- src/config/knexfile_kmq.js
+++ src/config/knexfile_kmq.js
@@ -2,7 +2,7 @@ const resolve = require("path").resolve;
 require('dotenv').config({path: resolve(__dirname, "../../.env")});
 
 module.exports = {
-  client: 'mysql',
+  client: 'mysql2',
   connection: {
     user: process.env.DB_USER, password: process.env.DB_PASS, database: "kmq", host: process.env.DB_HOST, charset: "utf8mb4", port: parseInt(process.env.DB_PORT), multipleStatements: true
   },
