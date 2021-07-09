7ef0c7c960bf97874548c491219427be89b8c1ac
Taaha Mahdi <mahditaaha11@gmail.com>
2021-07-09 00:05:59 -0400

    Allow group exclusion with ,groups (#841)
    * Allow group exclusion with ,groups
    
    * Replace set difference/intersection with lodash impl

--- src/commands/game_options/groups.ts
+++ src/commands/game_options/groups.ts
@@ -4,12 +4,13 @@ import { getGuildPreference, getMatchingGroupNames } from "../../helpers/game_ut
 import _logger from "../../logger";
 import { GameOption } from "../../types";
 import MessageContext from "../../structures/message_context";
+import { setIntersection } from "../../helpers/utils";
 
 const logger = _logger("groups");
 export default class GroupsCommand implements BaseCommand {
     help = {
         name: "groups",
-        description: "Select as many groups that you would like to hear from, separated by commas. A list of group names can be found [here](https://raw.githubusercontent.com/Brainicism/KMQ_Discord/master/data/group_list.txt)",
+        description: "Select as many groups that you would like to hear from, separated by commas. A list of group names can be found [here](https://raw.githubusercontent.com/Brainicism/KMQ_Discord/master/data/group_list.txt).",
         usage: ",groups [group1],{group2}",
         examples: [
             {
@@ -45,16 +46,29 @@ export default class GroupsCommand implements BaseCommand {
             }
         }
         const groupNames = parsedMessage.argument.split(",").map((groupName) => groupName.trim());
-        const { matchedGroups, unmatchedGroups } = await getMatchingGroupNames(groupNames);
+        const groups = await getMatchingGroupNames(groupNames);
+        let { matchedGroups } = groups;
+        const { unmatchedGroups } = groups;
         if (unmatchedGroups.length) {
             logger.info(`${getDebugLogHeader(message)} | Attempted to set unknown groups. groups =  ${unmatchedGroups.join(", ")}`);
             await sendErrorMessage(MessageContext.fromMessage(message), { title: "Unknown Group Name",
-                description: `One or more of the specified group names was not recognized. Those groups that matched are added. Please ensure that the group name matches exactly with the list provided by \`${process.env.BOT_PREFIX}help groups\` \nThe following groups were **not** recognized:\n ${unmatchedGroups.join(", ")} \nUse \`${process.env.BOT_PREFIX}add\` to add the unmatched groups.`,
+                description: `One or more of the specified group names was not recognized. Those groups that matched are added. Please ensure that the group name matches exactly with the list provided by \`${process.env.BOT_PREFIX}help groups\`. \nThe following groups were **not** recognized:\n ${unmatchedGroups.join(", ")} \nUse \`${process.env.BOT_PREFIX}add\` to add the unmatched groups.`,
                 footerText: groupsWarning });
         }
+
+        if (guildPreference.isExcludesMode()) {
+            const intersection = setIntersection(matchedGroups.map((x) => x.name), guildPreference.getExcludesGroupNames());
+            matchedGroups = matchedGroups.filter((x) => !intersection.has(x.name));
+            if (intersection.size > 0) {
+                sendErrorMessage(MessageContext.fromMessage(message), { title: "Groups and Exclude Conflict", description: `One or more of the given \`groups\` is already included in \`exclude\`. \nThe following groups were **not** added to \`groups\`:\n ${[...intersection].filter((x) => !x.includes("+")).join(", ")} \nUse \`${process.env.BOT_PREFIX}remove exclude\` and then \`${process.env.BOT_PREFIX}groups\` to allow them to play.` });
+                return;
+            }
+        }
+
         if (matchedGroups.length === 0) {
             return;
         }
+
         await guildPreference.setGroups(matchedGroups);
         await sendOptionsMessage(MessageContext.fromMessage(message), guildPreference, { option: GameOption.GROUPS, reset: false });
         logger.info(`${getDebugLogHeader(message)} | Groups set to ${guildPreference.getDisplayedGroupNames()}`);
