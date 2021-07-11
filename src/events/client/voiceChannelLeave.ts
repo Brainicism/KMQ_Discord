939df0dba69c0e72eceed7267652db5db205c634
taahamahdi <mahditaaha11@gmail.com>
2021-07-11 00:14:27 -0400

    Use endSession helper on voiceChannelLeave

--- src/events/client/voiceChannelLeave.ts
+++ src/events/client/voiceChannelLeave.ts
@@ -1,13 +1,14 @@
 import Eris from "eris";
 import state from "../../kmq";
 import { checkBotIsAlone } from "../../helpers/discord_utils";
+import { endSession } from "../../helpers/game_utils";
 
 export default async function voiceChannelLeaveHandler(member: Eris.Member, oldChannel: Eris.VoiceChannel) {
     const guildID = oldChannel.guild.id;
     if (checkBotIsAlone(guildID)) {
         const gameSession = state.gameSessions[guildID];
         if (gameSession) {
-            gameSession.endSession();
+            endSession(gameSession);
         }
     }
 }
