8f6fbdc259f6c1b6671bbfbdf910f44fddc6110d
Brian Le <brainicism@gmail.com>
2021-08-03 14:15:52 -0700

    Only abort on uncaught errors in CI (#887)

--- src/events/process/uncaughtException.ts
+++ src/events/process/uncaughtException.ts
@@ -1,9 +1,12 @@
 import { state } from "../../kmq";
 import { IPCLogger } from "../../logger";
+import { EnvType } from "../../types";
 
 const logger = new IPCLogger("uncaughtException");
 
 export default function uncaughtExceptionHandler(err: Error) {
     logger.error(`Uncaught Exception. Reason: ${err}. Trace: ${err.stack}`);
-    state.ipc.admiralBroadcast("abort");
+    if (process.env.NODE_ENV === EnvType.CI) {
+        state.ipc.admiralBroadcast("abort");
+    }
 }
