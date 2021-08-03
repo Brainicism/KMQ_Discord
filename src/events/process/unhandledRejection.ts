d24a9154da9cffe37567f7a91808b9b883c704b9
Brian Le <brainicism@gmail.com>
2021-08-03 14:14:48 -0700

    Only abort on uncaught errors in CI

--- src/events/process/unhandledRejection.ts
+++ src/events/process/unhandledRejection.ts
@@ -1,9 +1,12 @@
 import { state } from "../../kmq";
 import { IPCLogger } from "../../logger";
+import { EnvType } from "../../types";
 
 const logger = new IPCLogger("unhandledRejection");
 
 export default function unhandledRejectionHandler(reason: Error, p: Promise<any>) {
     logger.error(`Unhandled Rejection at: Promise ${p}. Reason: ${reason}. Trace: ${reason.stack}`);
-    state.ipc.admiralBroadcast("abort");
+    if (process.env.NODE_ENV === EnvType.CI) {
+        state.ipc.admiralBroadcast("abort");
+    }
 }
