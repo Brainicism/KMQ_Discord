70c73a46af9f1ce0831591422b6d9d22448522b0
taahamahdi <mahditaaha11@gmail.com>
2021-08-07 03:30:37 -0400

    Add early return if not in VC

--- src/commands/game_commands/play.ts
+++ src/commands/game_commands/play.ts
@@ -71,7 +71,7 @@ export default class PlayCommand implements BaseCommand {
     help = {
         name: "play",
         description: "Starts a game of KMQ. Pick between classic (default), elimination mode, and teams mode",
-        usage: ",play {classic | elimination | teams}",
+        usage: ",play {classic | elimination | teams}\n,play mc [easy | medium | hard]",
         priority: 1050,
         examples: [
             {
@@ -89,6 +89,10 @@ export default class PlayCommand implements BaseCommand {
             {
                 example: "`,play teams`",
                 explanation: "Split up into as many teams as you want and see who you can depend on to help you win!",
+            },
+            {
+                example: "`,play mc medium`",
+                explanation: "Start a multiple choice game where the choices are medium difficulty.",
             },
         ],
     };
@@ -109,62 +113,64 @@ export default class PlayCommand implements BaseCommand {
                     description: `Send \`${process.env.BOT_PREFIX}play\` again when you are in a voice channel.`,
                 });
             logger.warn(`${getDebugLogHeader(message)} | User not in voice channel`);
-        } else {
-            if (!voicePermissionsCheck(message)) {
-                return;
-            }
+            return;
+        }
 
-            const isEliminationMode = parsedMessage.components.length >= 1 && parsedMessage.components[0].toLowerCase() === "elimination";
-            const isTeamsMode = parsedMessage.components.length >= 1 && parsedMessage.components[0].toLowerCase() === "teams";
-            if (gameSessions[message.guildID] && !gameSessions[message.guildID].sessionInitialized && (isEliminationMode || isTeamsMode)) {
-                // User sent ,play elimination or ,play teams twice, reset the GameSession
-                deleteGameSession(message.guildID);
-            }
+        if (!voicePermissionsCheck(message)) {
+            return;
+        }
 
-            const messageContext = MessageContext.fromMessage(message);
-            if (!gameSessions[message.guildID] || (!isEliminationMode && !gameSessions[message.guildID].sessionInitialized)) {
-                // (1) No game session exists yet (create CLASSIC, ELIMINATION, or TEAMS game), or
-                // (2) User attempting to ,play after a ,play elimination/teams that didn't start, start CLASSIC game
-                const textChannel = channel;
-                let gameSession: GameSession;
+        const isEliminationMode = parsedMessage.components.length >= 1 && parsedMessage.components[0].toLowerCase() === "elimination";
+        const isTeamsMode = parsedMessage.components.length >= 1 && parsedMessage.components[0].toLowerCase() === "teams";
 
-                const gameOwner = KmqMember.fromUser(message.author);
-                if (isEliminationMode) {
-                    // (1) ELIMINATION game creation
-                    const lives = parsedMessage.components.length > 1 ? parseInt(parsedMessage.components[1]) : DEFAULT_LIVES;
-                    const startTitle = `\`${process.env.BOT_PREFIX}join\` the game and start it with \`${process.env.BOT_PREFIX}begin\`!`;
-                    const gameInstructions = `Type \`${process.env.BOT_PREFIX}join\` to play in the upcoming elimination game. Once all have joined, ${bold(gameOwner.tag)} must send \`${process.env.BOT_PREFIX}begin\` to start the game. Everyone begins with \`${lives}\` lives.`;
-                    gameSession = new GameSession(textChannel.id, voiceChannel.id, textChannel.guild.id, gameOwner, GameType.ELIMINATION, lives);
-                    gameSession.addEliminationParticipant(gameOwner);
-                    await sendInfoMessage(messageContext, { title: startTitle, description: gameInstructions, thumbnailUrl: KmqImages.HAPPY });
-                } else if (isTeamsMode) {
-                    // (1) TEAMS game creation
-                    const startTitle = `\`${process.env.BOT_PREFIX}join\` a team!`;
-                    const gameInstructions = `Team leaders, type \`${process.env.BOT_PREFIX}join [team name]\` to form a new team. Remember, switching teams mid-game will forfeit all your current score and EXP.`;
-                    await sendInfoMessage(messageContext, { title: startTitle, description: gameInstructions, thumbnailUrl: KmqImages.HAPPY });
-                    gameSession = new GameSession(textChannel.id, voiceChannel.id, textChannel.guild.id, gameOwner, GameType.TEAMS);
-                } else {
-                    // (1 and 2) CLASSIC game creation
-                    if (gameSessions[message.guildID]) {
-                        // (2) Let the user know they're starting a non-elimination/teams game
-                        const oldGameType = gameSessions[message.guildID].gameType;
-                        const prefix = process.env.BOT_PREFIX;
-                        const ignoringOldGameTypeTitle = `Ignoring \`${prefix}play ${oldGameType}\``;
-                        const gameSpecificInstructions = oldGameType === GameType.ELIMINATION ? `\`${prefix}join\` the game` : `\`${prefix}join [team name]\` a team`;
-                        const oldGameTypeInstructions = `If you meant to start a \`${oldGameType}\` game, \`${prefix}end\` this game, call \`${prefix}play ${oldGameType}\`, ${gameSpecificInstructions}, and then call \`${prefix}begin\`.`;
-                        sendErrorMessage(messageContext, { title: ignoringOldGameTypeTitle, description: oldGameTypeInstructions, thumbnailUrl: KmqImages.DEAD });
-                    }
+        if (gameSessions[message.guildID] && !gameSessions[message.guildID].sessionInitialized && (isEliminationMode || isTeamsMode)) {
+            // User sent ,play elimination or ,play teams twice, reset the GameSession
+            deleteGameSession(message.guildID);
+        }
 
-                    gameSession = new GameSession(textChannel.id, voiceChannel.id, textChannel.guild.id, gameOwner, GameType.CLASSIC);
-                    await sendBeginGameMessage(textChannel.name, voiceChannel.name, message, getCurrentVoiceMembers(voiceChannel.id));
-                    gameSession.startRound(guildPreference, messageContext);
-                    logger.info(`${getDebugLogHeader(message)} | Game session starting`);
+        const messageContext = MessageContext.fromMessage(message);
+        if (!gameSessions[message.guildID] || (!isEliminationMode && !gameSessions[message.guildID].sessionInitialized)) {
+            // (1) No game session exists yet (create CLASSIC, ELIMINATION, or TEAMS game), or
+            // (2) User attempting to ,play after a ,play elimination/teams that didn't start, start CLASSIC game
+            const textChannel = channel;
+            let gameSession: GameSession;
+
+            const gameOwner = KmqMember.fromUser(message.author);
+            if (isEliminationMode) {
+                // (1) ELIMINATION game creation
+                const lives = parsedMessage.components.length > 1 ? parseInt(parsedMessage.components[1]) : DEFAULT_LIVES;
+                const startTitle = `\`${process.env.BOT_PREFIX}join\` the game and start it with \`${process.env.BOT_PREFIX}begin\`!`;
+                const gameInstructions = `Type \`${process.env.BOT_PREFIX}join\` to play in the upcoming elimination game. Once all have joined, ${bold(gameOwner.tag)} must send \`${process.env.BOT_PREFIX}begin\` to start the game. Everyone begins with \`${lives}\` lives.`;
+                gameSession = new GameSession(textChannel.id, voiceChannel.id, textChannel.guild.id, gameOwner, GameType.ELIMINATION, lives);
+                gameSession.addEliminationParticipant(gameOwner);
+                await sendInfoMessage(messageContext, { title: startTitle, description: gameInstructions, thumbnailUrl: KmqImages.HAPPY });
+            } else if (isTeamsMode) {
+                // (1) TEAMS game creation
+                const startTitle = `\`${process.env.BOT_PREFIX}join\` a team!`;
+                const gameInstructions = `Team leaders, type \`${process.env.BOT_PREFIX}join [team name]\` to form a new team. Remember, switching teams mid-game will forfeit all your current score and EXP.`;
+                await sendInfoMessage(messageContext, { title: startTitle, description: gameInstructions, thumbnailUrl: KmqImages.HAPPY });
+                gameSession = new GameSession(textChannel.id, voiceChannel.id, textChannel.guild.id, gameOwner, GameType.TEAMS);
+            } else {
+                // (1 and 2) CLASSIC game creation
+                if (gameSessions[message.guildID]) {
+                    // (2) Let the user know they're starting a non-elimination/teams game
+                    const oldGameType = gameSessions[message.guildID].gameType;
+                    const prefix = process.env.BOT_PREFIX;
+                    const ignoringOldGameTypeTitle = `Ignoring \`${prefix}play ${oldGameType}\``;
+                    const gameSpecificInstructions = oldGameType === GameType.ELIMINATION ? `\`${prefix}join\` the game` : `\`${prefix}join [team name]\` a team`;
+                    const oldGameTypeInstructions = `If you meant to start a \`${oldGameType}\` game, \`${prefix}end\` this game, call \`${prefix}play ${oldGameType}\`, ${gameSpecificInstructions}, and then call \`${prefix}begin\`.`;
+                    sendErrorMessage(messageContext, { title: ignoringOldGameTypeTitle, description: oldGameTypeInstructions, thumbnailUrl: KmqImages.DEAD });
                 }
 
-                gameSessions[message.guildID] = gameSession;
-            } else {
-                await sendErrorMessage(messageContext, { title: "Game already in session" });
+                gameSession = new GameSession(textChannel.id, voiceChannel.id, textChannel.guild.id, gameOwner, GameType.CLASSIC);
+                await sendBeginGameMessage(textChannel.name, voiceChannel.name, message, getCurrentVoiceMembers(voiceChannel.id));
+                gameSession.startRound(guildPreference, messageContext);
+                logger.info(`${getDebugLogHeader(message)} | Game session starting`);
             }
+
+            gameSessions[message.guildID] = gameSession;
+        } else {
+            await sendErrorMessage(messageContext, { title: "Game already in session" });
         }
     };
 }
