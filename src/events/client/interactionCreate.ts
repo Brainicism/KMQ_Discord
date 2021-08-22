5f43049d2f4f44aeedf430ca2d767bd5076ea3d7
taahamahdi <mahditaaha11@gmail.com>
2021-08-22 04:48:43 -0400

    Code review changes
    * Remove interactionContext
    * Abstract interaction createMessage into 2 functions

--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -18,13 +18,53 @@ const getDebugLogHeader = ((interaction: Eris.ComponentInteraction | Eris.Comman
 
 const withinInteractionInterval = ((interaction: Eris.ComponentInteraction | Eris.CommandInteraction) => new Date().getTime() - interaction.createdAt <= MAX_INTERACTION_RESPONSE_TIME);
 
-const tryAcknowledge = (async (interaction: Eris.ComponentInteraction | Eris.CommandInteraction, interactionContext?: string) => {
+const tryAcknowledge = (async (interaction: Eris.ComponentInteraction | Eris.CommandInteraction) => {
     if (withinInteractionInterval(interaction)) {
         try {
             await interaction.acknowledge();
         } catch (err) {
-            logger.error(`${getDebugLogHeader(interaction)} | Interaction failed: ${interactionContext}. err = ${err.stack}`);
+            logger.error(`${getDebugLogHeader(interaction)} | Interaction acknowledge failed. err = ${err.stack}`);
         }
+    }
+});
+
+const tryCreateSuccessMessage = (async (interaction: Eris.ComponentInteraction | Eris.CommandInteraction, title: string, description: string) => {
+    try {
+        await interaction.createMessage({
+            embeds: [{
+                color: await userBonusIsActive(interaction.member?.id) ? EMBED_SUCCESS_BONUS_COLOR : EMBED_SUCCESS_COLOR,
+                author: {
+                    name: interaction.member?.username,
+                    icon_url: interaction.member?.avatarURL,
+                },
+                title: bold(title),
+                description,
+                thumbnail: { url: KmqImages.THUMBS_UP },
+            }],
+            flags: 64,
+        });
+    } catch (err) {
+        logger.error(`${getDebugLogHeader(interaction)} | Interaction acknowledge (success message) via createMessage failed. err = ${err.stack}`);
+    }
+});
+
+const tryCreateErrorMessage = (async (interaction: Eris.ComponentInteraction | Eris.CommandInteraction, description: string) => {
+    try {
+        await interaction.createMessage({
+            embeds: [{
+                color: EMBED_ERROR_COLOR,
+                author: {
+                    name: interaction.member?.username,
+                    icon_url: interaction.member?.avatarURL,
+                },
+                title: bold("Uh-oh"),
+                description,
+                thumbnail: { url: KmqImages.DEAD },
+            }],
+            flags: 64,
+        });
+    } catch (err) {
+        logger.error(`${getDebugLogHeader(interaction)} | Interaction acknowledge (failure message) via createMessage failed. err = ${err.stack}`);
     }
 });
 
@@ -33,7 +73,7 @@ export default async function interactionCreateHandler(interaction: Eris.PingInt
         // Multiple choice
         const gameSession = state.gameSessions[interaction.guildID];
         if (!gameSession || !gameSession.gameRound) {
-            await tryAcknowledge(interaction, `!gameSession = ${!gameSession}. If false, then !gameSession.gameRound = true`);
+            await tryAcknowledge(interaction);
             return;
         }
 
@@ -44,73 +84,23 @@ export default async function interactionCreateHandler(interaction: Eris.PingInt
         );
 
         if (!getCurrentVoiceMembers(gameSession.voiceChannelID).map((x) => x.id).includes(interaction.member.id)) {
-            await tryAcknowledge(interaction, "Interactor is not in voice channel");
+            await tryAcknowledge(interaction);
             return;
         }
 
         if (gameSession.gameRound.incorrectMCGuessers.has(interaction.member.id) && withinInteractionInterval(interaction)) {
-            try {
-                await interaction.createMessage({
-                    embeds: [{
-                        color: EMBED_ERROR_COLOR,
-                        author: {
-                            name: messageContext.author.username,
-                            icon_url: messageContext.author.avatarUrl,
-                        },
-                        title: bold("Uh-oh"),
-                        description: "You've already been eliminated this round.",
-                        thumbnail: { url: KmqImages.DEAD },
-                    }],
-                    flags: 64,
-                });
-            } catch (err) {
-                logger.error(`${getDebugLogHeader(interaction)} | Interaction failed: createMessage ack failed on user that has already been eliminated. err = ${err.stack}`);
-            }
-
+            await tryCreateErrorMessage(interaction, "You've already been eliminated this round.");
             return;
         }
 
         if (!gameSession.gameRound.isValidInteractionGuess(interaction.data.custom_id) && withinInteractionInterval(interaction)) {
-            try {
-                await interaction.createMessage({
-                    embeds: [{
-                        color: EMBED_ERROR_COLOR,
-                        author: {
-                            name: messageContext.author.username,
-                            icon_url: messageContext.author.avatarUrl,
-                        },
-                        title: bold("Uh-oh"),
-                        description: "You are attempting to pick an option from an already completed round.",
-                        thumbnail: { url: KmqImages.DEAD },
-                    }],
-                    flags: 64,
-                });
-            } catch (err) {
-                logger.error(`${getDebugLogHeader(interaction)} | Interaction failed: createMessage ack failed on interaction from previous round. err = ${err.stack}`);
-            }
-
+            await tryCreateErrorMessage(interaction, "You are attempting to pick an option from an already completed round.");
             return;
         }
 
         if (!gameSession.gameRound.isCorrectInteractionAnswer(interaction.data.custom_id)) {
             if (withinInteractionInterval(interaction)) {
-                try {
-                    await interaction.createMessage({
-                        embeds: [{
-                            color: EMBED_ERROR_COLOR,
-                            author: {
-                                name: messageContext.author.username,
-                                icon_url: messageContext.author.avatarUrl,
-                            },
-                            title: bold("Incorrect guess"),
-                            description: "You've been eliminated this round.",
-                            thumbnail: { url: KmqImages.DEAD },
-                        }],
-                        flags: 64,
-                    });
-                } catch (err) {
-                    logger.error(`${getDebugLogHeader(interaction)} | Interaction failed: createMessage ack failed on elimination message. err = ${err.stack}`);
-                }
+                await tryCreateErrorMessage(interaction, "You've been eliminated this round.");
             }
 
             if (!gameSession.gameRound) {
@@ -125,7 +115,7 @@ export default async function interactionCreateHandler(interaction: Eris.PingInt
             return;
         }
 
-        await tryAcknowledge(interaction, "Interactor guessed correctly");
+        await tryAcknowledge(interaction);
 
         const guildPreference = await getGuildPreference(messageContext.guildID);
         if (!gameSession.gameRound) return;
@@ -135,46 +125,12 @@ export default async function interactionCreateHandler(interaction: Eris.PingInt
         const gameSession = state.gameSessions[interaction.guildID];
         const song = gameSession?.getSongFromMessageID(interaction.data.target_id);
         if ((!gameSession || !song) && withinInteractionInterval(interaction)) {
-            const description = !gameSession ? "You can only bookmark songs during a game." : `You can only bookmark songs recently played in the last ${BOOKMARK_MESSAGE_SIZE} rounds. You must bookmark the message sent by the bot containing the song.`;
-            try {
-                interaction.createMessage({
-                    embeds: [{
-                        color: EMBED_ERROR_COLOR,
-                        author: interaction.member ? {
-                            name: interaction.member.username,
-                            icon_url: interaction.member.avatarURL,
-                        } : null,
-                        title: bold("Bookmark Error"),
-                        description,
-                        thumbnail: { url: KmqImages.DEAD },
-                    }],
-                    flags: 64,
-                });
-            } catch (err) {
-                logger.error(`${getDebugLogHeader(interaction)} | Interaction failed: createMessage ack failed on bookmarking old song. !gameSession = ${!gameSession}. If false, then !song = true. err = ${err.stack}`);
-            }
-
+            await tryCreateErrorMessage(interaction, !gameSession ? "You can only bookmark songs during a game." : `You can only bookmark songs recently played in the last ${BOOKMARK_MESSAGE_SIZE} rounds. You must bookmark the message sent by the bot containing the song.`);
             return;
         }
 
         if (withinInteractionInterval(interaction)) {
-            try {
-                interaction.createMessage({
-                    embeds: [{
-                        color: await userBonusIsActive(interaction.member.id) ? EMBED_SUCCESS_BONUS_COLOR : EMBED_SUCCESS_COLOR,
-                        author: interaction.member ? {
-                            name: interaction.member.username,
-                            icon_url: interaction.member.avatarURL,
-                        } : null,
-                        title: bold("Song Bookmarked"),
-                        description: `You'll receive a direct message with a link to ${bold(song.originalSongName)} at the end of the game.`,
-                        thumbnail: { url: KmqImages.THUMBS_UP },
-                    }],
-                    flags: 64,
-                });
-            } catch (err) {
-                logger.error(`${getDebugLogHeader(interaction)} | Interaction failed: createMessage ack failed on successful bookmark. err = ${err.stack}`);
-            }
+            await tryCreateSuccessMessage(interaction, "Song Bookmarked", `You'll receive a direct message with a link to ${bold(song.originalSongName)} at the end of the game.`);
         }
 
         gameSession.addBookmarkedSong(interaction.member?.id, song);
