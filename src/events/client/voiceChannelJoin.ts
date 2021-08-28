391a350b88bb772300783362ca9cb958dcb3dcb4
taahamahdi <mahditaaha11@gmail.com>
2021-08-28 01:23:43 -0400

    Early return in VC join if no gameSession

--- src/events/client/voiceChannelJoin.ts
+++ src/events/client/voiceChannelJoin.ts
@@ -5,6 +5,10 @@ import { isUserPremium, getGuildPreference } from "../../helpers/game_utils";
 export default async function voiceChannelJoinHandler(member: Eris.Member, newChannel: Eris.VoiceChannel) {
     const guildID = newChannel.guild.id;
     const gameSession = state.gameSessions[guildID];
+    if (!gameSession) {
+        return;
+    }
+
     if (newChannel.id === gameSession.voiceChannelID && await isUserPremium(member.id)) {
         await gameSession.addPremiumParticipant(member.id, await getGuildPreference(guildID));
     }
