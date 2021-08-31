3e2eb7e92115795d3c4dc3979cb7cfe85ded93cf
taahamahdi <mahditaaha11@gmail.com>
2021-08-31 18:50:38 -0400

    Make user_id primary key

--- src/migrations/20210821155505_premium_users.js
+++ src/migrations/20210821155505_premium_users.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable("premium_users", (table) => {
-        table.string("user_id").notNullable();
+        table.string("user_id");
         table.boolean("active").notNullable();
         table.datetime("pledge_relationship_start");
         table.datetime("last_charge_date");
-        table.unique(["user_id"]);
+        table.primary(["user_id"]);
     });
 };
 
