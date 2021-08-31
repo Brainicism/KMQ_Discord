4ad8017126cb9006ae7e8fa94e152c9d21db33de
Brian Le <brainicism@gmail.com>
2021-08-31 14:38:10 -0700

    Use primary VARCHAR index instead of TEXT subpart index on guild_preferences

---
+++ src/migrations/20210831203402_guild_preference_primary_idx.js
@@ -0,0 +1,18 @@
+
+exports.up = function (knex) {
+    return knex.schema.table("guild_preferences", function (table) {
+        table.dropIndex(["guild_id"], "guild_preferences_id_idx");
+        table.string("guild_id", 64).alter();
+        table.primary(["guild_id"]);
+    });
+};
+
+
+
+exports.down = function (knex) {
+    return knex.schema.table("guild_preferences", async function (table) {
+        await table.dropPrimary();
+        await table.text("guild_id").alter();
+        return knex.raw("ALTER TABLE guild_preferences ADD INDEX guild_preferences_id_idx(guild_id(20));")
+    });
+};
