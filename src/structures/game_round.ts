e7fa114f90cf50219f5d4e57c013741cc78c951f
Brian Le <brainicism@gmail.com>
2021-10-12 23:49:11 -0700

    Add random game round EXP bonuses

--- src/structures/game_round.ts
+++ src/structures/game_round.ts
@@ -82,6 +82,9 @@ export default class GameRound {
     /** The song release year */
     public readonly songYear: number;
 
+    /** Whether round will grant bonus EXP */
+    public readonly isBonus: boolean;
+
     /** List of players who have opted to skip the current GameRound */
     public skippers: Set<string>;
 
@@ -157,6 +160,7 @@ export default class GameRound {
         this.incorrectMCGuessers = new Set();
         this.interactionComponents = [];
         this.interactionMessage = null;
+        this.isBonus = Math.random() < 0.01;
     }
 
     /**
