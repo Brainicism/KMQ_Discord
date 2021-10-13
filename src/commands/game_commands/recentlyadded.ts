e23425b266a61f8620de2e6ee9b17df6980ea965
Taaha Mahdi <mahditaaha11@gmail.com>
2021-10-13 16:13:12 -0400

    Add command to show songs added in past 2 weeks (#1065)
    * Add command to show songs added in past 2 weeks
    
    * Add ,recentlyadded to GAMEPLAY
    
    * Fix footer null, eris-pagination upstream causing it
    
    Co-authored-by: Brian Le <brainicism@gmail.com>

---
+++ src/commands/game_commands/recentlyadded.ts
@@ -0,0 +1,58 @@
+import { EmbedOptions } from "eris";
+import BaseCommand, { CommandArgs } from "../interfaces/base_command";
+import { QueriedSong } from "../../types";
+import dbContext from "../../database_context";
+import { getDebugLogHeader, sendPaginationedEmbed, sendInfoMessage } from "../../helpers/discord_utils";
+import { standardDateFormat, chunkArray, friendlyFormattedNumber } from "../../helpers/utils";
+import { KmqImages } from "../../constants";
+import { IPCLogger } from "../../logger";
+import MessageContext from "../../structures/message_context";
+
+const logger = new IPCLogger("recentlyadded");
+
+const FIELDS_PER_EMBED = 9;
+
+export default class RecentlyAddedCommand implements BaseCommand {
+    help = {
+        name: "recentlyadded",
+        description: "View songs added to KMQ in the past two weeks.",
+        usage: ",recentlyadded",
+        examples: [
+            {
+                example: "`,recentlyadded`",
+                explanation: "Show recently added songs",
+            },
+        ],
+        priority: 30,
+    };
+
+    aliases = ["recent"];
+
+    call = async ({ message }: CommandArgs) => {
+        const newSongs: Array<QueriedSong> = await dbContext.kmq("available_songs")
+            .select(["clean_song_name AS songName", "song_name AS originalSongName", "artist_name AS artist", "link AS youtubeLink", "publishedon AS publishDate", "views"])
+            .orderBy("publishedon", "DESC")
+            .where("publishedon", ">=", standardDateFormat(new Date(Date.now() - 1000 * 60 * 60 * 24 * 14)));
+
+        if (newSongs.length === 0) {
+            sendInfoMessage(MessageContext.fromMessage(message), { title: "No Songs Recently Added", description: "Check back later to see if KMQ has added new songs.", thumbnailUrl: KmqImages.NOT_IMPRESSED });
+            return;
+        }
+
+        const fields = newSongs.map((song) => ({
+            name: `"${song.originalSongName}" - ${song.artist}`,
+            value: `Released ${standardDateFormat(song.publishDate)}\n[${friendlyFormattedNumber(song.views)} views](https://youtu.be/${song.youtubeLink})`,
+            inline: true,
+        }));
+
+        const embedFieldSubsets = chunkArray(fields, FIELDS_PER_EMBED);
+        const embeds: Array<EmbedOptions> = embedFieldSubsets.map((embedFieldsSubset) => ({
+            title: "Recently Added Songs",
+            description: "The following songs were added to KMQ in the past two weeks:",
+            fields: embedFieldsSubset,
+        }));
+
+        await sendPaginationedEmbed(message, embeds, null);
+        logger.info(`${getDebugLogHeader(message)} | Recently added songs retrieved.`);
+    };
+}
--- src/scripts/get-unclean-song-names.ts
+++ src/scripts/get-unclean-song-names.ts
@@ -6,7 +6,7 @@ const logger = new IPCLogger("get-unclean-song-names");
 
 (async () => {
     const songs: Array<QueriedSong> = await dbContext.kmq("available_songs")
-        .select(["song_name as name", "artist_name as artist", "link as youtubeLink"]);
+        .select(["song_name as songName", "artist_name as artist", "link as youtubeLink"]);
 
     // eslint-disable-next-line no-control-regex
     const nonAsciiSongs = songs.filter((x) => !/^[\x00-\x7F’]*$/.test(x.songName.split("(")[0].trim()));
