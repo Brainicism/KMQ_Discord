f7f2eb685950a276b8709c92168ebb7db220a1a8
taahamahdi <mahditaaha11@gmail.com>
2021-10-15 13:32:41 -0400

    Remove pipe in footer, reorder views to before

--- src/helpers/discord_utils.ts
+++ src/helpers/discord_utils.ts
@@ -385,15 +385,19 @@ export async function sendEndRoundMessage(messageContext: MessageContext,
 
     if (guessModeType === GuessModeType.ARTIST) {
         if (gameRound.artistAliases.length > 0) {
-            footer.text += `Aliases: ${Array.from(gameRound.artistAliases).join(", ")}\n`;
+            footer.text = `Aliases: ${Array.from(gameRound.artistAliases).join(", ")}`;
         }
     } else {
         if (gameRound.songAliases.length > 0) {
-            footer.text += `Aliases: ${Array.from(gameRound.songAliases).join(", ")}\n`;
+            footer.text = `Aliases: ${Array.from(gameRound.songAliases).join(", ")}`;
         }
     }
 
     if (timeRemaining) {
+        if (footer.text) {
+            footer.text += "\n";
+        }
+
         footer.text += timeRemaining > 0 ? `⏰ ${Math.ceil(timeRemaining)} minute(s) remaining` : "⏰ Time's up!";
     }
 
@@ -465,7 +469,7 @@ export async function sendEndRoundMessage(messageContext: MessageContext,
         description,
         thumbnailUrl: `https://img.youtube.com/vi/${gameRound.videoID}/hqdefault.jpg`,
         fields,
-        footerText: footer.text ? `${footer.text} | ${friendlyFormattedNumber(gameRound.views)} views` : `${friendlyFormattedNumber(gameRound.views)} views`,
+        footerText: `${friendlyFormattedNumber(gameRound.views)} views${footer.text ? `\n${footer.text}` : ""}`,
     }, correctGuess && !isMultipleChoiceMode,
     false);
 }
