ae355a34880c8f93c9452d2c779404c2bd7706ed
Brian Le <brainicism@gmail.com>
2021-12-01 18:24:21 -0800

    Add logging to redeploy_prod.sh

--- .github/workflows/redeploy_prod.sh
+++ .github/workflows/redeploy_prod.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 cd /home/kmq/prod
+
+echo "git fetch"
 git fetch
+
+echo "Checking out prod branch"
 git checkout prod
+
+echo "Pulling latest prod changes"
 git pull
-/home/kmq/.nvm/versions/node/v14.2.0/bin/npx ts-node src/scripts/announce-restart.ts
+
+echo "Latest commit:"
+git log -n 1 --pretty
+
+echo "Initiating restart"
+npx ts-node src/scripts/announce-restart.ts
