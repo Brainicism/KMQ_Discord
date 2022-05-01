bad476a14f467d89136d2fa2ea34c5b2eb8757e9
Brian Le <brainicism@gmail.com>
2022-05-01 21:11:37 +0000

    Add script to check for corrupt .ogg files

---
+++ src/scripts/check_corrupt_audio_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: ./check_audio_files.sh /path/to/audio/files"
+  exit 1
+fi
+
+bad_files=()
+for file in $1/*; do
+  if [[ "$(file ${file} | tee /dev/tty)" != "${file}: Ogg data, Opus audio," ]]; then
+    bad_files+=($file)
+  fi
+done
+echo "=============Bad files============"
+printf '%s\n' "${bad_files[@]}"
