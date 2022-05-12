2cf634a9474d3eb8570f5d413bfffb423cb417ae
Brian Le <brainicism@gmail.com>
2022-05-12 03:22:43 +0000

    Add translation key checker to gci-tests

--- src/ci_checks/missing_i18n.ts
+++ src/ci_checks/missing_i18n.ts
@@ -8,12 +8,24 @@ const logger = new IPCLogger("missing_i18n");
 
 function getNodeKeys(node: Node): Array<string> {
     const keys = new Set<string>();
-    if (node.getText().startsWith("LocalizationManager.localizer") && node.kind === SyntaxKind.CallExpression) {
-        for (const child of node.getChildren().flatMap((x) => x.getChildren())) {
+    if (
+        node.getText().startsWith("LocalizationManager.localizer") &&
+        node.kind === SyntaxKind.CallExpression
+    ) {
+        for (const child of node
+            .getChildren()
+            .flatMap((x) => x.getChildren())) {
             if (child.kind === SyntaxKind.StringLiteral) {
                 keys.add(child.getText());
-            } else if ([SyntaxKind.BinaryExpression, SyntaxKind.ConditionalExpression].includes(child.kind)) {
-                for (const nestedChild of child.getChildren().filter((x) => x.kind === SyntaxKind.StringLiteral)) {
+            } else if (
+                [
+                    SyntaxKind.BinaryExpression,
+                    SyntaxKind.ConditionalExpression,
+                ].includes(child.kind)
+            ) {
+                for (const nestedChild of child
+                    .getChildren()
+                    .filter((x) => x.kind === SyntaxKind.StringLiteral)) {
                     keys.add(nestedChild.getText());
                 }
             }
@@ -49,8 +61,8 @@ function getNodeKeys(node: Node): Array<string> {
 
     const keysArray = Array.from(keys).map((x) => x.slice(1, -1));
     const localizationManager = new LocalizationManager();
-    const missingKeys = keysArray.filter((key) =>
-        !localizationManager.hasKey(key)
+    const missingKeys = keysArray.filter(
+        (key) => !localizationManager.hasKey(key)
     );
 
     if (missingKeys.length > 0) {
