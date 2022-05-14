4a6c995d1239364c5a0dcde3c8f01b7889cd7800
Brian Le <brainicism@gmail.com>
2022-05-14 03:30:14 +0000

    Fixes

--- src/helpers/patreon_manager.ts
+++ src/helpers/patreon_manager.ts
@@ -6,19 +6,76 @@ import type Patron from "../interfaces/patron";
 
 const logger = new IPCLogger("patreon_manager");
 
-interface PatronResponse {
-    attributes: {
-        patron_status: string;
-        pledge_relationship_start?: Date;
-        social_connections: {
-            discord: string;
+interface PatreonResponse {
+    data: Array<{
+        attributes: {
+            patron_status: string;
+            pledge_relationship_start?: Date;
+            social_connections: {
+                discord: string;
+            };
+            user: {
+                data: {
+                    id: string;
+                };
+            };
         };
-    };
+        relationships: {
+            user: {
+                data: {
+                    id: string;
+                };
+            };
+        };
+        type: string;
+    }>;
+
+    included: Array<{
+        attributes: {
+            social_connections: {
+                discord: {
+                    user_id: string;
+                };
+            };
+        };
+        id: string;
+        type: string;
+    }>;
 }
 
 enum PatronState {
     ACTIVE = "active_patron",
     DECLINED = "declined_patron",
+}
+
+function parsePatreonResponse(patreonResponse: PatreonResponse): Array<Patron> {
+    const patrons: Array<Patron> = [];
+    for (const data of patreonResponse.data) {
+        if (data.type !== "member") continue;
+        const patreonMemberID = data.relationships.user.data.id;
+        const matchedPatreonUser = patreonResponse.included.find(
+            (x) => x.type === "user" && x.id === patreonMemberID
+        );
+
+        if (!matchedPatreonUser) {
+            logger.error(
+                `Couldn't find corresponding Patreon user for ${patreonMemberID}`
+            );
+            continue;
+        }
+
+        patrons.push({
+            discordID:
+                matchedPatreonUser.attributes.social_connections.discord
+                    .user_id,
+            activePatron: data.attributes.patron_status === PatronState.ACTIVE,
+            firstSubscribed: new Date(
+                data.attributes.pledge_relationship_start
+            ),
+        });
+    }
+
+    return patrons;
 }
 
 /**
@@ -32,38 +89,30 @@ export default async function updatePremiumUsers(): Promise<void> {
         return;
     }
 
-    let fetchedPatrons: Array<PatronResponse>;
+    let fetchedPatrons: Array<Patron>;
     try {
-        fetchedPatrons = (
+        const response: PatreonResponse = (
             await Axios.get(
-                `https://www.patreon.com/api/oauth2/v2/campaigns/${process.env.PATREON_CAMPAIGN_ID}/members`,
+                `https://www.patreon.com/api/oauth2/v2/campaigns/${
+                    process.env.PATREON_CAMPAIGN_ID
+                }/members${encodeURI(
+                    "?include=user,currently_entitled_tiers&fields[member]=patron_status,pledge_relationship_start&fields[user]=social_connections"
+                )}`,
                 {
-                    params: {
-                        include: "user,currently_entitled_tiers",
-                        fields: {
-                            member: "patron_status",
-                        },
-                    },
                     headers: {
                         Authorization: `Bearer ${process.env.PATREON_CREATOR_ACCESS_TOKEN}`,
                     },
                 }
             )
-        ).data.data;
+        ).data;
+
+        fetchedPatrons = parsePatreonResponse(response);
     } catch (err) {
         logger.error(`Failed fetching patrons. err = ${err}`);
         return;
     }
 
-    const patrons: Array<Patron> = fetchedPatrons
-        .filter(
-            (x: PatronResponse) => !!x.attributes.social_connections.discord
-        )
-        .map((x: PatronResponse) => ({
-            activePatron: x.attributes.patron_status === PatronState.ACTIVE,
-            discordID: x.attributes.social_connections.discord,
-            firstSubscribed: x.attributes.pledge_relationship_start,
-        }));
+    const patrons: Array<Patron> = fetchedPatrons.filter((x) => !!x.discordID);
 
     const activePatronIDs: string[] = patrons
         .filter((x) => x.activePatron)
