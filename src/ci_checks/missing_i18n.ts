604c6d597f924288b8d91dc7cfd02fd22b75c5e3
taahamahdi <mahditaaha11@gmail.com>
2022-05-14 02:31:40 -0400

    Code review changes

--- src/ci_checks/missing_i18n.ts
+++ src/ci_checks/missing_i18n.ts
@@ -1,21 +1,25 @@
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable @typescript-eslint/quotes */
+
 import { IPCLogger } from "../logger";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { ScriptTarget, SyntaxKind, createSourceFile } from "typescript";
-import { readFileSync } from "fs";
+import {
+    ModuleKind,
+    ScriptTarget,
+    SyntaxKind,
+    createProgram,
+} from "typescript";
 import LocalizationManager from "../helpers/localization_manager";
 import type { CallExpression, Node } from "typescript";
 
 const logger = new IPCLogger("missing_i18n");
 
 const dynamicTranslationKeyAllowlist = [
-    // eslint-disable-next-line no-template-curly-in-string
     "`command.locale.language.${DEFAULT_LOCALE}`",
-    // eslint-disable-next-line no-template-curly-in-string
     "`command.locale.language.${language}`",
     "highestRankTitle.title",
     "rankTitle.title",
     "RANK_TITLES[0].title",
-    // eslint-disable-next-line @typescript-eslint/quotes
     'x["badge_name"]',
     "endGameMessage.title",
     "endGameMessage.message",
@@ -35,87 +39,83 @@ const translationInternalFunctions = [
     "LocalizationManager.internalLocalizer.t",
 ];
 
-function getNodeKeys(node: Node): Array<string> {
-    const keys = new Set<string>();
-    const interfaceTranslation = translationInterfaceFunctions.some((x) =>
-        node.getText().startsWith(x)
-    );
+const translationKeys = new Set<string>();
+const dynamicTranslationKeys = new Set<string>();
 
-    const internalTranslation = translationInternalFunctions.some((x) =>
-        node.getText().startsWith(x)
-    );
-
-    const translationNode = interfaceTranslation || internalTranslation;
-
-    if (translationNode && node.kind === SyntaxKind.CallExpression) {
+function getNodeKeys(node: Node): void {
+    if (node.kind === SyntaxKind.CallExpression) {
         const expression = node as CallExpression;
-        const key = interfaceTranslation
-            ? expression.arguments[1]
-            : expression.arguments[0];
+        const interfaceTranslation = translationInterfaceFunctions.some(
+            (x) => expression.expression.getText() === x
+        );
 
-        const keyText = key.getText();
+        const internalTranslation = translationInternalFunctions.some(
+            (x) => expression.expression.getText() === x
+        );
 
-        if (key.kind === SyntaxKind.StringLiteral) {
-            if (keyText.includes("plural")) {
-                keys.add(`${keyText.slice(0, -1)}_one"`);
-                keys.add(`${keyText.slice(0, -1)}_other"`);
-            } else {
-                keys.add(keyText);
+        if (interfaceTranslation || internalTranslation) {
+            const translationKeyNode = interfaceTranslation
+                ? expression.arguments[1]
+                : expression.arguments[0];
+
+            const keyText =
+                translationKeyNode.kind === SyntaxKind.StringLiteral
+                    ? translationKeyNode.getText().slice(1, -1)
+                    : translationKeyNode.getText();
+
+            if (translationKeyNode.kind === SyntaxKind.StringLiteral) {
+                if (keyText.startsWith("misc.plural")) {
+                    translationKeys.add(`${keyText}_one`);
+                    translationKeys.add(`${keyText}_other`);
+                } else {
+                    translationKeys.add(keyText);
+                }
+            } else if (
+                [
+                    SyntaxKind.BinaryExpression,
+                    SyntaxKind.ConditionalExpression,
+                ].includes(translationKeyNode.kind)
+            ) {
+                for (const nestedChild of translationKeyNode.getChildren()) {
+                    getNodeKeys(nestedChild);
+                }
+            } else if (!dynamicTranslationKeyAllowlist.includes(keyText)) {
+                dynamicTranslationKeys.add(keyText);
             }
-        } else if (
-            [
-                SyntaxKind.BinaryExpression,
-                SyntaxKind.ConditionalExpression,
-            ].includes(key.kind)
-        ) {
-            for (const nestedChild of key
-                .getChildren()
-                .filter((x) => x.kind === SyntaxKind.StringLiteral)) {
-                keys.add(nestedChild.getText());
-            }
-        } else if (!dynamicTranslationKeyAllowlist.includes(keyText)) {
-            logger.error(`"${keyText}" is not in the dynamic allow list`);
         }
     }
 
     for (const child of node.getChildren()) {
-        const childKeys = getNodeKeys(child);
-        for (const key of childKeys) {
-            keys.add(key);
-        }
+        getNodeKeys(child);
     }
-
-    return Array.from(keys);
 }
 
 (() => {
     const filenames = process.argv.slice(2);
-    const keys = new Set<string>();
-    for (const file of filenames) {
-        const sourceFile = createSourceFile(
-            file,
-            readFileSync(file).toString(),
-            ScriptTarget.ES2019,
-            true
-        );
+    const program = createProgram(filenames, {
+        target: ScriptTarget.ES2019,
+        module: ModuleKind.CommonJS,
+    });
 
-        const fileKeys = getNodeKeys(sourceFile);
-        for (const key of fileKeys) {
-            keys.add(key);
-        }
+    program.getTypeChecker();
+    for (const sourceFile of program.getSourceFiles()) {
+        getNodeKeys(sourceFile);
     }
 
-    const keysArray = Array.from(keys).map((x) => x.slice(1, -1));
     const localizationManager = new LocalizationManager();
-    const missingKeys = keysArray.filter(
+    const missingKeys = Array.from(translationKeys).filter(
         (key) => !localizationManager.hasKey(key)
     );
 
-    if (missingKeys.length > 0) {
-        for (const missingKey of missingKeys) {
-            logger.error(`"${missingKey}" is missing`);
-        }
+    for (const missingKey of missingKeys) {
+        logger.error(`"${missingKey}" is missing`);
+    }
 
+    for (const missingKey of dynamicTranslationKeys) {
+        logger.error(`"${missingKey}" is not in the dynamic allow list`);
+    }
+
+    if (missingKeys.length > 0 || dynamicTranslationKeys.size > 0) {
         process.exit(1);
     }
 })();
