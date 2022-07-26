79b8b6543c289351a16d7f06055545b7bddb439b
Brian Le <brainicism@gmail.com>
2022-07-26 01:55:05 +0000

    Slash command for ,forceskip

--- src/commands/game_commands/forceskip.ts
+++ src/commands/game_commands/forceskip.ts
@@ -8,6 +8,8 @@ import {
 } from "../../helpers/discord_utils";
 import { getMention } from "../../helpers/utils";
 import CommandPrechecks from "../../command_prechecks";
+import Eris from "eris";
+import LocaleType from "../../enums/locale_type";
 import LocalizationManager from "../../helpers/localization_manager";
 import MessageContext from "../../structures/message_context";
 import Session from "../../structures/session";
@@ -36,66 +38,139 @@ export default class ForceSkipCommand implements BaseCommand {
         priority: 1009,
     });
 
+    slashCommands = (): Array<Eris.ChatInputApplicationCommandStructure> => [
+        {
+            name: "forceskip",
+            description: LocalizationManager.localizer.translate(
+                LocaleType.EN,
+                "command.forceskip.help.description"
+            ),
+            type: Eris.Constants.ApplicationCommandTypes.CHAT_INPUT,
+        },
+    ];
+
     call = async ({ message }: CommandArgs): Promise<void> => {
+        await ForceSkipCommand.executeForceSkip(
+            MessageContext.fromMessage(message)
+        );
+    };
+
+    static async executeForceSkip(
+        messageContext: MessageContext,
+        interaction?: Eris.CommandInteraction
+    ): Promise<void> {
         if (
-            !areUserAndBotInSameVoiceChannel(message.author.id, message.guildID)
+            !areUserAndBotInSameVoiceChannel(
+                messageContext.author.id,
+                messageContext.guildID
+            )
         ) {
+            await sendErrorMessage(
+                messageContext,
+                {
+                    title: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "command.forceskip.failure.notOwner.title"
+                    ),
+                    description: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "misc.preCheck.differentVC"
+                    ),
+                },
+                interaction
+            );
+
             logger.warn(
                 `${getDebugLogHeader(
-                    message
+                    messageContext
                 )} | Invalid forceskip. User and bot are not in the same voice channel.`
             );
             return;
         }
 
-        const session = Session.getSession(message.guildID);
+        const session = Session.getSession(messageContext.guildID);
         if (
             !session.round ||
             session.round.skipAchieved ||
             session.round.finished
         ) {
+            sendErrorMessage(
+                messageContext,
+                {
+                    title: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "misc.failure.game.noneInProgress.title"
+                    ),
+                    description: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "misc.failure.game.noneInProgress.description"
+                    ),
+                    thumbnailUrl: KmqImages.NOT_IMPRESSED,
+                },
+                interaction
+            );
             return;
         }
 
-        if (message.author.id !== session.owner.id) {
-            await sendErrorMessage(MessageContext.fromMessage(message), {
-                title: LocalizationManager.localizer.translate(
-                    message.guildID,
-                    "command.forceskip.failure.notOwner.title"
-                ),
-                description: LocalizationManager.localizer.translate(
-                    message.guildID,
-                    "command.forceskip.failure.notOwner.description",
-                    { mentionedUser: getMention(session.owner.id) }
-                ),
-            });
+        if (messageContext.author.id !== session.owner.id) {
+            await sendErrorMessage(
+                messageContext,
+                {
+                    title: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "command.forceskip.failure.notOwner.title"
+                    ),
+                    description: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "command.forceskip.failure.notOwner.description",
+                        { mentionedUser: getMention(session.owner.id) }
+                    ),
+                },
+                interaction
+            );
             return;
         }
 
         session.round.skipAchieved = true;
         sendInfoMessage(
-            MessageContext.fromMessage(message),
+            messageContext,
             {
                 color: EMBED_SUCCESS_COLOR,
                 title: LocalizationManager.localizer.translate(
-                    message.guildID,
+                    messageContext.guildID,
                     "misc.skip"
                 ),
                 description: LocalizationManager.localizer.translate(
-                    message.guildID,
+                    messageContext.guildID,
                     "command.forceskip.description"
                 ),
                 thumbnailUrl: KmqImages.NOT_IMPRESSED,
             },
-            true
+            true,
+            null,
+            [],
+            interaction
         );
 
-        await session.endRound(MessageContext.fromMessage(message), {
+        await session.endRound(messageContext, {
             correct: false,
         });
 
-        await session.startRound(MessageContext.fromMessage(message));
+        await session.startRound(messageContext);
         session.lastActiveNow();
-        logger.info(`${getDebugLogHeader(message)} | Owner force-skipped.`);
-    };
+        logger.info(
+            `${getDebugLogHeader(messageContext)} | Owner force-skipped.`
+        );
+    }
+
+    /**
+     * @param interaction - The interaction
+     * @param messageContext - The message context
+     */
+    async processChatInputInteraction(
+        interaction: Eris.CommandInteraction,
+        messageContext: MessageContext
+    ): Promise<void> {
+        await ForceSkipCommand.executeForceSkip(messageContext, interaction);
+    }
 }
