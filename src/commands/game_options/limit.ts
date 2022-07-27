4c8f09c117477ef920799cd04c94cb97c8ce3981
Brian Le <brainicism@gmail.com>
2022-07-27 10:03:23 -0400

    Slash command for ,limit (#1440)
    * Slash command for ,limit
    
    * Fix reset
    
    * Add missing description

--- src/commands/game_options/limit.ts
+++ src/commands/game_options/limit.ts
@@ -2,12 +2,15 @@ import { DEFAULT_LIMIT } from "../../constants";
 import { IPCLogger } from "../../logger";
 import {
     getDebugLogHeader,
+    getInteractionOptionValueInteger,
     sendErrorMessage,
     sendOptionsMessage,
 } from "../../helpers/discord_utils";
 import CommandPrechecks from "../../command_prechecks";
+import Eris from "eris";
 import GameOption from "../../enums/game_option_name";
 import GuildPreference from "../../structures/guild_preference";
+import LocaleType from "../../enums/locale_type";
 import LocalizationManager from "../../helpers/localization_manager";
 import MessageContext from "../../structures/message_context";
 import Session from "../../structures/session";
@@ -16,6 +19,11 @@ import type CommandArgs from "../../interfaces/command_args";
 import type HelpDocumentation from "../../interfaces/help";
 
 const logger = new IPCLogger("limit");
+
+const LIMIT_START_MIN = 0;
+const LIMIT_START_MAX = 100000;
+const LIMIT_END_MIN = 1;
+const LIMIT_END_MAX = 100000;
 
 export default class LimitCommand implements BaseCommand {
     preRunChecks = [{ checkFn: CommandPrechecks.competitionPrecheck }];
@@ -27,14 +35,14 @@ export default class LimitCommand implements BaseCommand {
             {
                 name: "limit_1",
                 type: "number" as const,
-                minValue: 0,
-                maxValue: 100000,
+                minValue: LIMIT_START_MIN,
+                maxValue: LIMIT_START_MAX,
             },
             {
                 name: "limit_2",
                 type: "number" as const,
-                minValue: 1,
-                maxValue: 100000,
+                minValue: LIMIT_END_MIN,
+                maxValue: LIMIT_END_MAX,
             },
         ],
     };
@@ -77,71 +85,211 @@ export default class LimitCommand implements BaseCommand {
         priority: 140,
     });
 
+    slashCommands = (): Array<Eris.ChatInputApplicationCommandStructure> => [
+        {
+            name: "limit",
+            description: LocalizationManager.localizer.translate(
+                LocaleType.EN,
+                "command.limit.interaction.description"
+            ),
+            type: Eris.Constants.ApplicationCommandTypes.CHAT_INPUT,
+            options: [
+                {
+                    name: "top",
+                    description: LocalizationManager.localizer.translate(
+                        LocaleType.EN,
+                        "command.limit.interaction.description_top"
+                    ),
+                    type: Eris.Constants.ApplicationCommandOptionTypes
+                        .SUB_COMMAND,
+                    options: [
+                        {
+                            name: "limit",
+                            description:
+                                LocalizationManager.localizer.translate(
+                                    LocaleType.EN,
+                                    "command.limit.interaction.description_top"
+                                ),
+                            type: Eris.Constants.ApplicationCommandOptionTypes
+                                .INTEGER,
+                            required: true,
+                            max_value: LIMIT_END_MAX,
+                            min_value: LIMIT_END_MIN,
+                        } as any,
+                    ],
+                },
+                {
+                    name: "range",
+                    description: LocalizationManager.localizer.translate(
+                        LocaleType.EN,
+                        "command.limit.interaction.description_range"
+                    ),
+                    type: Eris.Constants.ApplicationCommandOptionTypes
+                        .SUB_COMMAND,
+                    options: [
+                        {
+                            name: "limit_start",
+                            description:
+                                LocalizationManager.localizer.translate(
+                                    LocaleType.EN,
+                                    "command.limit.interaction.description_range"
+                                ),
+                            type: Eris.Constants.ApplicationCommandOptionTypes
+                                .INTEGER,
+                            required: true,
+                            max_value: LIMIT_START_MAX,
+                            min_value: LIMIT_START_MIN,
+                        } as any,
+                        {
+                            name: "limit_end",
+                            description:
+                                LocalizationManager.localizer.translate(
+                                    LocaleType.EN,
+                                    "command.limit.interaction.description_range"
+                                ),
+                            type: Eris.Constants.ApplicationCommandOptionTypes
+                                .INTEGER,
+                            required: true,
+                            max_value: LIMIT_END_MAX,
+                            min_value: LIMIT_END_MIN,
+                        } as any,
+                    ],
+                },
+            ],
+        },
+    ];
+
     call = async ({ message, parsedMessage }: CommandArgs): Promise<void> => {
-        const guildPreference = await GuildPreference.getGuildPreference(
-            message.guildID
-        );
-
-        if (parsedMessage.components.length === 0) {
-            await guildPreference.reset(GameOption.LIMIT);
-            await sendOptionsMessage(
-                Session.getSession(message.guildID),
-                MessageContext.fromMessage(message),
-                guildPreference,
-                [{ option: GameOption.LIMIT, reset: true }]
-            );
-            logger.info(`${getDebugLogHeader(message)} | Limit reset.`);
-            return;
-        }
-
         let limitStart: number;
         let limitEnd: number;
-        if (parsedMessage.components.length === 1) {
+
+        if (parsedMessage.components.length === 0) {
+            limitStart = null;
+            limitEnd = null;
+        } else if (parsedMessage.components.length === 1) {
             limitStart = 0;
             limitEnd = parseInt(parsedMessage.components[0], 10);
-            if (limitEnd === 0) {
-                sendErrorMessage(MessageContext.fromMessage(message), {
-                    title: LocalizationManager.localizer.translate(
-                        message.guildID,
-                        "command.limit.failure.invalidLimit.title"
-                    ),
-                    description: LocalizationManager.localizer.translate(
-                        message.guildID,
-                        "command.limit.failure.invalidLimit.greaterThanZero.description"
-                    ),
-                });
-                return;
-            }
         } else {
             limitStart = parseInt(parsedMessage.components[0], 10);
             limitEnd = parseInt(parsedMessage.components[1], 10);
-            if (limitEnd <= limitStart) {
-                sendErrorMessage(MessageContext.fromMessage(message), {
+        }
+
+        await LimitCommand.updateOption(
+            MessageContext.fromMessage(message),
+            limitStart,
+            limitEnd
+        );
+    };
+
+    static async updateOption(
+        messageContext: MessageContext,
+        limitStart: number,
+        limitEnd: number,
+        interaction?: Eris.CommandInteraction
+    ): Promise<void> {
+        if (limitEnd === 0) {
+            sendErrorMessage(
+                messageContext,
+                {
                     title: LocalizationManager.localizer.translate(
-                        message.guildID,
+                        messageContext.guildID,
                         "command.limit.failure.invalidLimit.title"
                     ),
                     description: LocalizationManager.localizer.translate(
-                        message.guildID,
-                        "command.limit.failure.invalidLimit.greaterThanStart.description"
+                        messageContext.guildID,
+                        "command.limit.failure.invalidLimit.greaterThanZero.description"
                     ),
-                });
-                return;
-            }
+                },
+                interaction
+            );
+            return;
         }
 
-        await guildPreference.setLimit(limitStart, limitEnd);
-        await sendOptionsMessage(
-            Session.getSession(message.guildID),
-            MessageContext.fromMessage(message),
-            guildPreference,
-            [{ option: GameOption.LIMIT, reset: false }]
+        if (limitEnd <= limitStart) {
+            sendErrorMessage(
+                messageContext,
+                {
+                    title: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "command.limit.failure.invalidLimit.title"
+                    ),
+                    description: LocalizationManager.localizer.translate(
+                        messageContext.guildID,
+                        "command.limit.failure.invalidLimit.greaterThanStart.description"
+                    ),
+                },
+                interaction
+            );
+            return;
+        }
+
+        const guildPreference = await GuildPreference.getGuildPreference(
+            messageContext.guildID
         );
 
-        logger.info(
-            `${getDebugLogHeader(message)} | Limit set to ${
-                guildPreference.gameOptions.limitStart
-            } - ${guildPreference.gameOptions.limitEnd}`
+        const reset = limitStart === null && limitEnd === null;
+
+        if (reset) {
+            await guildPreference.reset(GameOption.LIMIT);
+            logger.info(`${getDebugLogHeader(messageContext)} | Limit reset.`);
+        } else {
+            await guildPreference.setLimit(limitStart, limitEnd);
+
+            logger.info(
+                `${getDebugLogHeader(messageContext)} | Limit set to ${
+                    guildPreference.gameOptions.limitStart
+                } - ${guildPreference.gameOptions.limitEnd}`
+            );
+        }
+
+        await sendOptionsMessage(
+            Session.getSession(messageContext.guildID),
+            messageContext,
+            guildPreference,
+            [{ option: GameOption.LIMIT, reset }],
+            null,
+            null,
+            null,
+            interaction
         );
-    };
+    }
+
+    /**
+     * @param interaction - The interaction
+     * @param messageContext - The message context
+     */
+    async processChatInputInteraction(
+        interaction: Eris.CommandInteraction,
+        messageContext: MessageContext
+    ): Promise<void> {
+        const limitDataOption = interaction.data
+            .options[0] as Eris.InteractionDataOptionsSubCommand;
+
+        let limitStart: number;
+        let limitEnd: number;
+        if (limitDataOption.name === "range") {
+            limitStart = getInteractionOptionValueInteger(
+                limitDataOption.options,
+                "limit_start"
+            );
+
+            limitEnd = getInteractionOptionValueInteger(
+                limitDataOption.options,
+                "limit_end"
+            );
+        } else {
+            limitStart = 0;
+            limitEnd = getInteractionOptionValueInteger(
+                limitDataOption.options,
+                "limit"
+            );
+        }
+
+        await LimitCommand.updateOption(
+            messageContext,
+            limitStart,
+            limitEnd,
+            interaction
+        );
+    }
 }
