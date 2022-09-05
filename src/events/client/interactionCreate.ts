5b414e3db218c1e15bb08501a6db1df51def58a6
Brian Le <brainicism@gmail.com>
2022-09-04 23:02:13 -0700

    Log interaction execution times (#1469)
    * Log interaction execution times
    
    * Fix measurement for autocomplete

--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -1,9 +1,11 @@
 import { BOOKMARK_COMMAND_NAME, PROFILE_COMMAND_NAME } from "../../constants";
 import { IPCLogger } from "../../logger";
 import {
+    getInteractionValue,
     tryCreateInteractionErrorAcknowledgement,
     tryInteractionAcknowledge,
 } from "../../helpers/discord_utils";
+import { measureExecutionTime } from "../../helpers/utils";
 import Eris from "eris";
 import GroupsCommand from "../../commands/game_options/groups";
 import KmqMember from "../../structures/kmq_member";
@@ -44,7 +46,8 @@ export default async function interactionCreateHandler(
     );
 
     const session = Session.getSession(interaction.guildID);
-
+    let interactionPromise: Promise<any> = null;
+    let interactionName: string = null;
     if (interaction instanceof Eris.ComponentInteraction) {
         if (
             !session ||
@@ -54,18 +57,16 @@ export default async function interactionCreateHandler(
             return;
         }
 
-        await session.handleComponentInteraction(interaction, messageContext);
-        return;
-    }
-
-    if (interaction instanceof Eris.CommandInteraction) {
+        interactionName = `Component interaction for '${interaction.data.custom_id}'`;
+        interactionPromise = session.handleComponentInteraction(
+            interaction,
+            messageContext
+        );
+    } else if (interaction instanceof Eris.CommandInteraction) {
         if (
             interaction.data.type ===
             Eris.Constants.ApplicationCommandTypes.CHAT_INPUT
         ) {
-            logger.info(
-                `CHAT_INPUT CommandInteraction received for '${interaction.data.name}'`
-            );
             const commandInteractionHandler =
                 State.client.commands[interaction.data.name];
 
@@ -86,80 +87,92 @@ export default async function interactionCreateHandler(
                     }
                 }
 
-                await commandInteractionHandler.processChatInputInteraction(
-                    interaction,
-                    messageContext
-                );
+                interactionName = `CHAT_INPUT CommandInteraction interaction for '${interaction.data.name}'`;
+                interactionPromise =
+                    commandInteractionHandler.processChatInputInteraction(
+                        interaction,
+                        messageContext
+                    );
             } else {
                 logger.error(
                     `No handler found for CHAT_INPUT CommandInteraction: ${interaction.data.name}`
                 );
             }
+        } else {
+            switch (interaction.data.name) {
+                case PROFILE_COMMAND_NAME: {
+                    interaction = interaction as Eris.CommandInteraction;
+                    if (
+                        interaction.data.type ===
+                        Eris.Constants.ApplicationCommandTypes.USER
+                    ) {
+                        interactionName = `USER Application Command for '${interaction.data.name}'`;
+                        interactionPromise =
+                            ProfileCommand.handleProfileInteraction(
+                                interaction as Eris.CommandInteraction,
+                                interaction.data.target_id,
+                                true
+                            );
+                    } else if (
+                        interaction.data.type ===
+                        Eris.Constants.ApplicationCommandTypes.MESSAGE
+                    ) {
+                        const messageID = interaction.data.target_id;
+                        const authorID = (
+                            interaction as Eris.CommandInteraction
+                        ).data.resolved["messages"].get(messageID).author.id;
 
-            return;
+                        interactionName = `MESSAGE Application Command for '${interaction.data.name}'`;
+
+                        interactionPromise =
+                            ProfileCommand.handleProfileInteraction(
+                                interaction,
+                                authorID,
+                                true
+                            );
+                    }
+
+                    break;
+                }
+
+                case BOOKMARK_COMMAND_NAME: {
+                    if (!session) {
+                        tryCreateInteractionErrorAcknowledgement(
+                            interaction as Eris.CommandInteraction,
+                            null,
+                            LocalizationManager.localizer.translate(
+                                interaction.guildID,
+                                "misc.failure.interaction.bookmarkOutsideGame"
+                            )
+                        );
+                        return;
+                    }
+
+                    interactionName = `Application Command for '${interaction.data.name}'`;
+                    interactionPromise = session.handleBookmarkInteraction(
+                        interaction as Eris.CommandInteraction
+                    );
+                    break;
+                }
+
+                default: {
+                    logger.error(
+                        `No handler found for CommandInteraction  (type = ${interaction.data.type}): ${interaction.data.name}`
+                    );
+                }
+            }
         }
     } else if (interaction instanceof Eris.AutocompleteInteraction) {
         const autocompleteInteractionHandler =
             AUTO_COMPLETE_COMMAND_INTERACTION_HANDLERS[interaction.data.name];
 
+        const parsedInteraction = getInteractionValue(interaction);
         if (autocompleteInteractionHandler) {
-            await autocompleteInteractionHandler(interaction);
-            return;
+            interactionName = `Autocomplete interaction for '${interaction.data.name}' for value '${parsedInteraction.focusedKey}'`;
+            interactionPromise = autocompleteInteractionHandler(interaction);
         }
     }
 
-    switch (interaction.data.name) {
-        case PROFILE_COMMAND_NAME: {
-            interaction = interaction as Eris.CommandInteraction;
-            if (
-                interaction.data.type ===
-                Eris.Constants.ApplicationCommandTypes.USER
-            ) {
-                ProfileCommand.handleProfileInteraction(
-                    interaction as Eris.CommandInteraction,
-                    interaction.data.target_id,
-                    true
-                );
-            } else if (
-                interaction.data.type ===
-                Eris.Constants.ApplicationCommandTypes.MESSAGE
-            ) {
-                const messageID = interaction.data.target_id;
-                const authorID = (
-                    interaction as Eris.CommandInteraction
-                ).data.resolved["messages"].get(messageID).author.id;
-
-                ProfileCommand.handleProfileInteraction(
-                    interaction,
-                    authorID,
-                    true
-                );
-            }
-
-            break;
-        }
-
-        case BOOKMARK_COMMAND_NAME: {
-            if (!session) {
-                tryCreateInteractionErrorAcknowledgement(
-                    interaction as Eris.CommandInteraction,
-                    null,
-                    LocalizationManager.localizer.translate(
-                        interaction.guildID,
-                        "misc.failure.interaction.bookmarkOutsideGame"
-                    )
-                );
-                return;
-            }
-
-            session.handleBookmarkInteraction(
-                interaction as Eris.CommandInteraction
-            );
-            break;
-        }
-
-        default: {
-            break;
-        }
-    }
+    const executionTime = await measureExecutionTime(interactionPromise);
+    logger.info(` ${interactionName} took ${executionTime}ms`);
 }
