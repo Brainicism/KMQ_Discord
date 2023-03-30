b124fed143ecdc9d1d6385a34d4338085655238d
Brian Le <brainicism@gmail.com>
2023-03-30 04:44:47 +0000

    Add start_date index for game_sessions table

---
+++ src/migrations/20230330043817_game-sessions-index.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.raw(
+        "ALTER TABLE game_sessions ADD INDEX game_sessions_start_date_index(start_date);"
+    );
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.table("game_sessions", function (table) {
+        table.dropIndex(["start_date"], "game_sessions_start_date_index");
+    });
+};
