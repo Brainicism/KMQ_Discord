902b1bbd8e0eecc901b39fa1f69c4a12e74b1c21
Brian Le <brainicism@gmail.com>
2023-05-09 21:04:21 -0700

    Announce restart after standby is ready (#1651)

--- src/scripts/announce-restart.ts
+++ src/scripts/announce-restart.ts
@@ -36,110 +36,11 @@ async function abortRestart(): Promise<void> {
     );
 }
 
-const delay = (time: number): Promise<void> =>
-    // eslint-disable-next-line no-promise-executor-return
-    new Promise((res) => setTimeout(res, time));
-
-function serverShutdown(
+async function announceRestart(
     restartMinutes: number,
     restartDate: Date,
-    restart: boolean,
-    dockerImage: string
+    restart: boolean
 ): Promise<void> {
-    return new Promise((resolve, reject) => {
-        setInterval(() => {
-            console.log(
-                `Restarting in ${Math.floor(
-                    (restartDate.getTime() - Date.now()) / 1000
-                )} seconds`
-            );
-        }, 1000 * 10).unref();
-
-        setTimeout(async () => {
-            const appName = process.env.APP_NAME;
-            try {
-                if (!restart) {
-                    console.log("Stopping KMQ...");
-                    cp.execSync(`APP_NAME=${appName} npm run docker-stop`);
-                } else {
-                    const oldAppName = `${appName}-old`;
-                    console.log("Upgrading KMQ...");
-                    console.log("Renaming container...");
-                    cp.execSync(`docker rename ${appName} ${oldAppName}`);
-
-                    console.log(`Pulling new docker image: ${dockerImage}...`);
-                    cp.execSync(`docker pull ${dockerImage} `);
-
-                    console.log(
-                        "Provisioning standby container with new image..."
-                    );
-
-                    cp.execSync(
-                        `APP_NAME=${appName} IMAGE_NAME=${dockerImage} IS_STANDBY=true npm run docker-run`
-                    );
-
-                    let standbyProvisioning = true;
-                    const standbyCreateTime = Date.now();
-                    while (standbyProvisioning) {
-                        const standbyStdout = cp
-                            .execSync(
-                                `docker exec ${appName} /bin/sh -c "if [ -f "standby" ]; then cat standby; fi"`
-                            )
-                            .toString()
-                            .trim();
-
-                        console.log(
-                            `Standby Status: ${
-                                standbyStdout || "bootstrapping"
-                            }`
-                        );
-
-                        if (standbyStdout === "ready") {
-                            standbyProvisioning = false;
-                        }
-
-                        if (Date.now() - standbyCreateTime > 1000 * 60 * 5) {
-                            throw new Error(
-                                "Standby took too long to provision"
-                            );
-                        }
-
-                        await delay(1000);
-                    }
-
-                    // drop old primary
-                    console.log("Dropping old primary...");
-                    cp.execSync(`docker rm -f ${oldAppName}`);
-
-                    // promote standby to primary
-                    console.log("Promoting standby to primary...");
-                    cp.execSync(
-                        `docker exec ${appName} /bin/sh -c "rm standby"`
-                    );
-                }
-
-                resolve();
-            } catch (e) {
-                console.error(`Error while issuing restart command :${e}`);
-                await abortRestart();
-                reject(e);
-            }
-        }, restartMinutes * 1000 * 60);
-    });
-}
-
-process.on("SIGINT", async () => {
-    await abortRestart();
-    process.exit(0);
-});
-
-(async () => {
-    const options = program.opts();
-    const restartMinutes = options.timer;
-    const dockerImage = options.dockerImage;
-    const restartDate = new Date();
-    restartDate.setMinutes(restartDate.getMinutes() + restartMinutes);
-    console.log(options);
     try {
         await Axios.post(
             `http://127.0.0.1:${process.env.WEB_SERVER_PORT}/announce-restart`,
@@ -152,21 +53,120 @@ process.on("SIGINT", async () => {
                 },
             }
         );
+
+        const timer = setInterval(() => {
+            if (restartDate.getTime() - Date.now() < 0) {
+                clearInterval(timer);
+            }
+
+            console.log(
+                `Restarting in ${Math.floor(
+                    (restartDate.getTime() - Date.now()) / 1000
+                )} seconds`
+            );
+        }, 1000 * 10).unref();
+
+        console.log(
+            `Next ${
+                restart ? "restart" : "shutdown"
+            } scheduled at ${restartDate}`
+        );
     } catch (e) {
         console.error(`KMQ might not be up? ${e}`);
         process.exit(1);
     }
+}
 
-    console.log(
-        `Next ${
-            options.restart ? "restart" : "shutdown"
-        } scheduled at ${restartDate}`
-    );
+const delay = (time: number): Promise<void> =>
+    // eslint-disable-next-line no-promise-executor-return
+    new Promise((res) => setTimeout(res, time));
 
-    await serverShutdown(
-        restartMinutes,
-        restartDate,
-        options.restart,
-        dockerImage
-    );
+function serverShutdown(
+    restartMinutes: number,
+    restart: boolean,
+    dockerImage: string
+): Promise<void> {
+    return new Promise(async () => {
+        // if stopping server, inform immediately
+        const appName = process.env.APP_NAME;
+        if (!restart) {
+            const restartDate = new Date();
+            restartDate.setMinutes(restartDate.getMinutes() + restartMinutes);
+            await announceRestart(restartMinutes, restartDate, restart);
+
+            setTimeout(() => {
+                console.log("Stopping KMQ...");
+                cp.execSync(`APP_NAME=${appName} npm run docker-stop`);
+            }, restartMinutes * 1000 * 60);
+        } else {
+            const oldAppName = `${appName}-old`;
+            console.log("Upgrading KMQ...");
+            console.log("Renaming container...");
+            cp.execSync(`docker rename ${appName} ${oldAppName}`);
+
+            console.log(`Pulling new docker image: ${dockerImage}...`);
+            cp.execSync(`docker pull ${dockerImage} `);
+
+            console.log("Provisioning standby container with new image...");
+
+            cp.execSync(
+                `APP_NAME=${appName} IMAGE_NAME=${dockerImage} IS_STANDBY=true npm run docker-run`
+            );
+
+            let standbyProvisioning = true;
+            const standbyCreateTime = Date.now();
+            while (standbyProvisioning) {
+                const standbyStdout = cp
+                    .execSync(
+                        `docker exec ${appName} /bin/sh -c "if [ -f "standby" ]; then cat standby; fi"`
+                    )
+                    .toString()
+                    .trim();
+
+                console.log(
+                    `Standby Status: ${standbyStdout || "bootstrapping"}`
+                );
+
+                if (standbyStdout === "ready") {
+                    standbyProvisioning = false;
+                }
+
+                if (Date.now() - standbyCreateTime > 1000 * 60 * 5) {
+                    throw new Error("Standby took too long to provision");
+                }
+
+                await delay(1000);
+            }
+
+            // announce restart after new standby has provisioned
+            const restartDate = new Date();
+            restartDate.setMinutes(restartDate.getMinutes() + restartMinutes);
+            await announceRestart(restartMinutes, restartDate, restart);
+
+            setTimeout(() => {
+                // drop old primary
+                console.log("Dropping old primary...");
+                cp.execSync(`docker rm -f ${oldAppName}`);
+
+                // promote standby to primary
+                console.log("Promoting standby to primary...");
+                cp.execSync(`docker exec ${appName} /bin/sh -c "rm standby"`);
+            }, restartMinutes * 1000 * 60);
+        }
+    });
+}
+
+process.on("SIGINT", async () => {
+    await abortRestart();
+    process.exit(0);
+});
+
+(async () => {
+    const options = program.opts();
+    const restartMinutes = options.timer;
+    const dockerImage = options.dockerImage;
+
+    console.log(options);
+
+    await serverShutdown(restartMinutes, options.restart, dockerImage);
 })();
