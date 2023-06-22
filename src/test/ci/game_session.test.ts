ca20aa4b5b7958007b11946d8f622cbc3ef4a909
taahamahdi <mahditaaha11@gmail.com>
2023-06-22 18:32:31 -0400

    (WIP) Add tests for streaks #1681

--- src/test/ci/game_session.test.ts
+++ src/test/ci/game_session.test.ts
@@ -151,5 +151,207 @@ describe("game session", () => {
                 assert.ok(sendEndGameMessageStub.called);
             });
         });
+
+        describe("streaks happy path", () => {
+            it("should keep track of a streak", async () => {
+                // round starts successfully
+                voiceChannelStub.voiceMembers.add({ id: "1" } as any);
+
+                for (let i = 0; i < 6; i++) {
+                    await gameSession.startRound(
+                        new MessageContext("id", null, "guild_id")
+                    );
+                    gameSession.connection = voiceConnection;
+                    assert.ok(prepareRoundSpy.called);
+                    assert.ok(ensureVoiceConnectionSpy.called);
+                    assert.ok(playSongSpy.called);
+                    assert.ok(endSessionSpy.notCalled);
+                    assert.ok(endRoundStub.notCalled);
+                    assert.ok(
+                        gameSession.songSelector.getSongs().countBeforeLimit > 0
+                    );
+                    assert(gameSession.round);
+
+                    const correctGuess =
+                        gameSession.round.acceptedSongAnswers[0];
+
+                    const gameStarter = new KmqMember("123");
+                    const messageContext = new MessageContext(
+                        "",
+                        gameStarter,
+                        "123",
+                        ""
+                    );
+
+                    // incorrect guesses
+                    for (let j = 0; j < 5; j++) {
+                        await gameSession.guessSong(
+                            messageContext,
+                            `badguess${j}`,
+                            Date.now()
+                        );
+                        assert.ok(endRoundStub.notCalled);
+                    }
+
+                    // correct guess
+                    await gameSession.guessSong(
+                        messageContext,
+                        correctGuess,
+                        Date.now()
+                    );
+
+                    assert.ok(
+                        endRoundStub.calledWith(messageContext, {
+                            correct: true,
+                            correctGuessers: gameSession.round.correctGuessers,
+                        })
+                    );
+
+                    // Update gameStarter's points
+                    gameStarter.pointsAwarded += 1;
+                    assert.deepStrictEqual(gameSession.round.correctGuessers, [
+                        gameStarter,
+                    ]);
+                    const lastGuesser = (gameSession as any).lastGuesser;
+                    assert.strictEqual(lastGuesser.streak, i + 1);
+                    assert.strictEqual(lastGuesser.userID, gameStarter.id);
+                }
+
+                // end session
+                const sendEndGameMessageStub = sandbox.stub(
+                    gameSession,
+                    "sendEndGameMessage"
+                );
+
+                await gameSession.endSession("Initiated by test");
+                assert.ok(gameSession.finished);
+
+                assert.ok(sendEndGameMessageStub.called);
+            });
+        });
+
+        describe("streak reset path", () => {
+            it("should reset the streak and start a new one", async () => {
+                // round starts successfully
+                voiceChannelStub.voiceMembers.add({ id: "1" } as any);
+
+                for (let i = 0; i < 10; i++) {
+                    await gameSession.startRound(
+                        new MessageContext("id", null, "guild_id")
+                    );
+                    gameSession.connection = voiceConnection;
+                    assert.ok(prepareRoundSpy.called);
+                    assert.ok(ensureVoiceConnectionSpy.called);
+                    assert.ok(playSongSpy.called);
+                    assert.ok(endSessionSpy.notCalled);
+                    assert.ok(endRoundStub.notCalled);
+                    assert.ok(
+                        gameSession.songSelector.getSongs().countBeforeLimit > 0
+                    );
+                    assert(gameSession.round);
+
+                    const correctGuess =
+                        gameSession.round.acceptedSongAnswers[0];
+
+                    const gameStarter = new KmqMember("123");
+                    const gameStarterContext = new MessageContext(
+                        "",
+                        gameStarter,
+                        "123",
+                        ""
+                    );
+
+                    const otherGuesser = new KmqMember("456");
+                    const otherGuesserContext = new MessageContext(
+                        "",
+                        otherGuesser,
+                        "456",
+                        ""
+                    );
+
+                    // incorrect guesses
+                    for (let j = 0; j < 5; j++) {
+                        await gameSession.guessSong(
+                            gameStarterContext,
+                            `badguess${j}`,
+                            Date.now()
+                        );
+                        assert.ok(endRoundStub.notCalled);
+                    }
+
+                    for (let j = 0; j < 5; j++) {
+                        await gameSession.guessSong(
+                            otherGuesserContext,
+                            `badguess${j}`,
+                            Date.now()
+                        );
+                        assert.ok(endRoundStub.notCalled);
+                    }
+
+                    if (i < 3) {
+                        // gameStarter correct guess
+                        await gameSession.guessSong(
+                            gameStarterContext,
+                            correctGuess,
+                            Date.now()
+                        );
+
+                        assert.ok(
+                            endRoundStub.calledWith(gameStarterContext, {
+                                correct: true,
+                                correctGuessers:
+                                    gameSession.round.correctGuessers,
+                            })
+                        );
+
+                        // Update gameStarter's points
+                        gameStarter.pointsAwarded += 1;
+                        assert.deepStrictEqual(
+                            gameSession.round.correctGuessers,
+                            [gameStarter]
+                        );
+                        // const lastGuesser = (gameSession as any).lastGuesser;
+                        // assert.strictEqual(lastGuesser.streak, i + 1);
+                        // assert.strictEqual(lastGuesser.userID, gameStarter.id);
+                    } else {
+                        // otherGuesser correct guess
+                        await gameSession.guessSong(
+                            otherGuesserContext,
+                            correctGuess,
+                            Date.now()
+                        );
+
+                        assert.ok(
+                            endRoundStub.calledWith(gameStarterContext, {
+                                correct: true,
+                                correctGuessers:
+                                    gameSession.round.correctGuessers,
+                            })
+                        );
+
+                        // Update otherGuesser's points
+                        otherGuesser.pointsAwarded += 1;
+                        assert.deepStrictEqual(
+                            gameSession.round.correctGuessers,
+                            [otherGuesser]
+                        );
+                        const lastGuesser = (gameSession as any).lastGuesser;
+                        assert.strictEqual(lastGuesser.streak, i - 3 + 1);
+                        assert.strictEqual(lastGuesser.userID, otherGuesser.id);
+                    }
+                }
+
+                // end session
+                const sendEndGameMessageStub = sandbox.stub(
+                    gameSession,
+                    "sendEndGameMessage"
+                );
+
+                await gameSession.endSession("Initiated by test");
+                assert.ok(gameSession.finished);
+
+                assert.ok(sendEndGameMessageStub.called);
+            });
+        });
     });
 });
