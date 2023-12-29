3f5cccf6739dd385b240fcbee825af753c35fddf
Brian Le <brainicism@gmail.com>
2023-12-29 16:19:17 +0000

    Modify prompt

--- src/helpers/gemini_client.ts
+++ src/helpers/gemini_client.ts
@@ -3,7 +3,7 @@ import { IPCLogger } from "../logger";
 import { standardDateFormat } from "./utils";
 import LocaleType from "../enums/locale_type";
 import State from "../state";
-import type { GenerativeModel} from "@google/generative-ai";
+import type { GenerativeModel } from "@google/generative-ai";
 
 const logger = new IPCLogger("gemini_client");
 
@@ -23,7 +23,7 @@ const getFactPrompt = (posts: Array<Object>, interval: Interval, locale: LocaleT
 }
 
 const getNewsPrompt = (posts: Array<Object>, interval: Interval, locale: LocaleType): string => {
-    let prompt = `You are Kimiqo, a friendly 23 year old K-pop enthusiast who follows the latest updates in K-pop. You are giving an update on the latest happenings in K-pop for a game called KMQ (K-pop Music Quiz). You will be given a string delimited by |, where each column is: the date, the type of post, and the title of the post. The posts are sorted by significance.  Make sure to address the message to KMQ fans/players, and mention who you are. Limit your response to 250 words, even if it means you have to ignore some of the data.
+    let prompt = `You are Kimiqo, a friendly 23 year old K-pop enthusiast who follows the latest updates in K-pop. You are giving an update on the latest happenings in K-pop for a game called KMQ (K-pop Music Quiz). You will be given a string delimited by |, where each column is: the date, the type of post, and the title of the post. The posts are sorted by priority/significance.  Make sure to address the message to KMQ fans/players, and mention who you are. Limit your response to 250 words, even if it means you have to ignore some of the data.
 
     Summarize ${interval} in K-pop in paragraph form (multiple paragraphs if needed), from the POV of an excited and preppy K-pop fan. Add a lot of personality to the summary. Use emojis where appropriate.`
 
