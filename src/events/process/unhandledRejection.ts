af23be16198691c06839f28119f704a1b1f3d826
Brian Le <brainicism@gmail.com>
2024-02-09 10:49:18 -0800

    Logging severity changes (#1870)

--- src/events/process/unhandledRejection.ts
+++ src/events/process/unhandledRejection.ts
@@ -8,12 +8,20 @@ const logger = new IPCLogger("unhandledRejection");
  * Handles the 'unhandledRejection' event
  * @param err - Error object
  */
-export default function unhandledRejectionHandler(err: Error): void {
-    logger.error(
-        `Cluster Unhandled Rejection | Name: ${err.name}. Reason: ${
-            err.message
-        }. Trace: ${err.stack}}. Object: ${JSON.stringify(err)}`,
-    );
+export default function unhandledRejectionHandler(err: Object): void {
+    let message: string;
+    if (typeof err === "string") {
+        message = err;
+    } else if (err instanceof Error) {
+        message = `Name: ${err.name}. Reason: ${err.message}. Trace: ${err.stack}}`;
+    } else {
+        logger.warn(
+            `Unexpected parameter passed into unhandledRejectionHandler: ${err.constructor.name}}`,
+        );
+        message = JSON.stringify(err);
+    }
+
+    logger.error(`Cluster Unhandled Rejection | ${message}`);
     if (process.env.NODE_ENV === EnvType.CI) {
         State.ipc.sendToAdmiral("abort");
     }
