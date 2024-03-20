8c4d493952df6a651793adfc78b6c45da893190c
Taaha Mahdi <mahditaaha11@gmail.com>
2024-03-19 21:29:10 -0400

    Update i18n-ai-translate, use ChatGPT (#1967)
    * Update i18n-ai-translate, use ChatGPT
    
    * Update i18n-ai-translate with required engine/model
    
    * Model to gpt-4-turbo-preview

--- src/ci_checks/i18n-ai-translate.sh
+++ src/ci_checks/i18n-ai-translate.sh
@@ -7,7 +7,7 @@ cp i18n/en.json i18n/en-latest.json
 git checkout $merge_base -- i18n/en.json
 if ! diff i18n/en-latest.json i18n/en.json; then
     echo "Updating translations..."
-    npx i18n-ai-translate diff -b i18n/en.json -a i18n/en-latest.json -l "English" --verbose
+    npx i18n-ai-translate diff -b i18n/en.json -a i18n/en-latest.json -l "English" --verbose --engine chatgpt --model gpt-4-turbo-preview
 else
     echo "No changes to translations"
 fi
