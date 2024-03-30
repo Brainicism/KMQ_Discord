b46cee5ac261671443556cde204da1059a426337
Taaha Mahdi <mahditaaha11@gmail.com>
2024-03-30 03:17:27 -0400

    Lint

--- src/structures/game_round.ts
+++ src/structures/game_round.ts
@@ -367,53 +367,55 @@ export default class GameRound extends Round {
      */
     async interactionMarkAnswers(correctGuesses: number): Promise<void> {
         if (!this.interactionMessage) return;
-        const components = this.interactionComponents.map((x) => ({
-            type: 1,
-            components: x.components.map((y: Eris.InteractionButton) => {
-                if (
-                    Object.values(ClipAction).includes(
-                        y.custom_id as ClipAction,
-                    )
-                ) {
-                    // TODO heart here
+        const components: Eris.ActionRow[] = this.interactionComponents.map(
+            (x) => ({
+                type: 1,
+                components: x.components.map((y: Eris.InteractionButton) => {
+                    if (
+                        Object.values(ClipAction).includes(
+                            y.custom_id as ClipAction,
+                        )
+                    ) {
+                        // TODO heart here
+                        return {
+                            label: y.label,
+                            custom_id: y.custom_id,
+                            style: y.style,
+                            type: 2,
+                            disabled: true,
+                        };
+                    }
+
+                    const noGuesses =
+                        this.interactionIncorrectAnswerUUIDs[y.custom_id] === 0;
+
+                    let label = y.label;
+                    let style: 1 | 3 | 4;
+                    if (this.interactionCorrectAnswerUUID === y.custom_id) {
+                        if (correctGuesses) {
+                            label += ` (${correctGuesses})`;
+                        }
+
+                        style = 3;
+                    } else if (noGuesses) {
+                        style = 1;
+                    } else {
+                        label += ` (${
+                            this.interactionIncorrectAnswerUUIDs[y.custom_id]
+                        })`;
+                        style = 4;
+                    }
+
                     return {
-                        label: y.label,
+                        label,
                         custom_id: y.custom_id,
-                        style: y.style,
+                        style,
                         type: 2,
                         disabled: true,
                     };
-                }
-
-                const noGuesses =
-                    this.interactionIncorrectAnswerUUIDs[y.custom_id] === 0;
-
-                let label = y.label;
-                let style: 1 | 3 | 4;
-                if (this.interactionCorrectAnswerUUID === y.custom_id) {
-                    if (correctGuesses) {
-                        label += ` (${correctGuesses})`;
-                    }
-
-                    style = 3;
-                } else if (noGuesses) {
-                    style = 1;
-                } else {
-                    label += ` (${
-                        this.interactionIncorrectAnswerUUIDs[y.custom_id]
-                    })`;
-                    style = 4;
-                }
-
-                return {
-                    label,
-                    custom_id: y.custom_id,
-                    style,
-                    type: 2,
-                    disabled: true,
-                };
+                }),
             }),
-        }));
+        );
 
         try {
             await this.interactionMessage.edit({
