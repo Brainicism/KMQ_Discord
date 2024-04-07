381b5d24ee2a26d583f821511a4bfdd1497a69c9
Taaha Mahdi <mahditaaha11@gmail.com>
2024-04-07 00:19:54 -0400

    Add pre/post audio silence to clip game mode (#2051)
    * Add pre/post audio silence
    
    * Fix bug where round wasn't ended correctly
    
    ---------
    
    Co-authored-by: Brian Le <brainicism@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -166,7 +166,6 @@ export const ELIMINATION_DEFAULT_LIVES = 10;
 export const ELIMINATION_MAX_LIVES = 10000;
 export const ELIMINATION_MIN_LIVES = 1;
 
-export const CLIP_REPLAY_DELAY_MS = 100;
 export const CLIP_MIN_DURATION = 0.75;
 export const CLIP_DEFAULT_DURATION = 2;
 export const CLIP_MAX_DURATION = 15;
