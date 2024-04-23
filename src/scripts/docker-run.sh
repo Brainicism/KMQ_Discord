1827d29f1b684521c5d63d198b679710144a7fe2
Brian Le <brainicism@gmail.com>
2024-04-23 23:53:31 +0000

    Remove duplicate checks in docker-run.sh

--- src/scripts/docker-run.sh
+++ src/scripts/docker-run.sh
@@ -3,8 +3,6 @@
 source .env
 WEB_SERVER_PORT=${WEB_SERVER_PORT:-5858}
 echo "Preparing restart of $APP_NAME at port $WEB_SERVER_PORT..."
-result=$(curl -s 127.0.0.1:$WEB_SERVER_PORT/session-count)
-echo "$result session(s) are still active, waiting 10s..."
 while true; do
     result=$(curl -s 127.0.0.1:$WEB_SERVER_PORT/session-count)
 
