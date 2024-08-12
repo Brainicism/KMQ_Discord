3c17d2fdac8843ef6b06e7f19311f4bf7ac557dd
Brian Le <brainicism@gmail.com>
2024-08-12 19:29:03 +0000

    Disable yt-dlp autoupdates for now

--- src/scripts/download-new-songs.ts
+++ src/scripts/download-new-songs.ts
@@ -277,26 +277,27 @@ async function updateNotDownloaded(
 }
 
 async function getLatestYtDlpBinary(): Promise<void> {
-    // try {
-    //     await fs.promises.access(ytDlpLocation, fs.constants.F_OK);
-    // } catch (_err) {
-    //     logger.warn("yt-dlp binary doesn't exist, downloading...");
-    //     try {
-    //         await exec(
-    //             `curl -L https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/latest/download/yt-dlp -o ${ytDlpLocation}`,
-    //         );
-    //         await exec(`chmod u+x ${ytDlpLocation}`);
-    //     } catch (err) {
-    //         throw new Error(
-    //             `Failed to fetch latest yt-dlp library. err = ${err}`,
-    //         );
-    //     }
-    // }
-    // try {
-    //     await exec(`${ytDlpLocation} -U`);
-    // } catch (err) {
-    //     throw new Error(`Failed to update yt-dlp library. err = ${err}`);
-    // }
+    try {
+        await fs.promises.access(ytDlpLocation, fs.constants.F_OK);
+    } catch (_err) {
+        logger.warn("yt-dlp binary doesn't exist, downloading...");
+        try {
+            await exec(
+                `curl -L https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/latest/download/yt-dlp -o ${ytDlpLocation}`,
+            );
+            await exec(`chmod u+x ${ytDlpLocation}`);
+        } catch (err) {
+            throw new Error(
+                `Failed to fetch latest yt-dlp library. err = ${err}`,
+            );
+        }
+    }
+
+    try {
+        await exec(`${ytDlpLocation} -U`);
+    } catch (err) {
+        throw new Error(`Failed to update yt-dlp library. err = ${err}`);
+    }
 }
 
 const downloadNewSongs = async (
@@ -370,7 +371,7 @@ const downloadNewSongs = async (
     logger.info(`Total songs to be downloaded: ${songsToDownload.length}`);
 
     try {
-        await getLatestYtDlpBinary();
+        // await getLatestYtDlpBinary();
     } catch (err) {
         logger.warn(`Failed to get latest yt-dlp binary. err = ${err}`);
     }
