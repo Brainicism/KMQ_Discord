0a945d7abd4b5bcdd9e50594dbdc80a02d528d85
Brian Le <brainicism@gmail.com>
2024-08-14 18:41:33 +0000

    Support visitor ID cookies

--- src/scripts/session-generator.sh
+++ src/scripts/session-generator.sh
@@ -8,11 +8,15 @@ session_data=$(docker run quay.io/invidious/youtube-trusted-session-generator)
 visitor_data=$(echo "$session_data" | grep -oP 'visitor_data: \K.*')
 po_token=$(echo "$session_data" | grep -oP 'po_token: \K.*')
 generated_at=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+visitor_id=$(echo "${visitor_data:0:18}" | base64 --decode | tr -d '[:space:]')
+
 json_output=$(jq -n \
                   --arg visitor_data "$visitor_data" \
                   --arg po_token "$po_token" \
                   --arg generated_at "$generated_at" \
-                  '{visitor_data: $visitor_data, po_token: $po_token, generated_at: $generated_at}')
+                  --arg visitor_id "$visitor_id" \
+                  '{visitor_data: $visitor_data, po_token: $po_token, generated_at: $generated_at, visitor_id: $visitor_id}')
+
 
 output_file="$1"
 echo "$json_output" > "$output_file"
