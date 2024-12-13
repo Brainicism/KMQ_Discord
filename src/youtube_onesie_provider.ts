121ae05c707031348f64b65c5ad15c88e2e323e7
Brian Le <brainicism@gmail.com>
2024-12-13 22:21:35 +0000

    Prettier

--- src/youtube_onesie_provider.ts
+++ src/youtube_onesie_provider.ts
@@ -108,34 +108,38 @@ export default class YoutubeOnesieProvider {
         args: OnesieRequestArgs,
     ): Promise<OnesieRequest> {
         const { videoId, poToken, clientConfig, innertube } = args;
-  const { clientKeyData, encryptedClientKey, onesieUstreamerConfig } = clientConfig;
-  const clonedInnerTubeContext: Context = structuredClone(innertube.session.context);
+        const { clientKeyData, encryptedClientKey, onesieUstreamerConfig } =
+            clientConfig;
+        const clonedInnerTubeContext: Context = structuredClone(
+            innertube.session.context,
+        );
 
-  // Change or remove these if you want to use a different client. I chose TVHTML5 purely for testing.
-  clonedInnerTubeContext.client.clientName = Constants.CLIENTS.TV.NAME;
-  clonedInnerTubeContext.client.clientVersion = Constants.CLIENTS.TV.VERSION;
-  
-  const params: Record<string, any> = {
-    playbackContext: {
-      contentPlaybackContext: {
-        vis: 0,
-        splay: false,
-        lactMilliseconds: '-1',
-        signatureTimestamp: innertube.session.player?.sts
-      }
-    },
-    videoId
-  };
-  
-  if (poToken) {
-    params.serviceIntegrityDimensions = {};
-    params.serviceIntegrityDimensions.poToken = poToken;
-  }
-  
-  const playerRequestJson = {
-    context: clonedInnerTubeContext,
-    ...params
-  };
+        // Change or remove these if you want to use a different client. I chose TVHTML5 purely for testing.
+        clonedInnerTubeContext.client.clientName = Constants.CLIENTS.TV.NAME;
+        clonedInnerTubeContext.client.clientVersion =
+            Constants.CLIENTS.TV.VERSION;
+
+        const params: Record<string, any> = {
+            playbackContext: {
+                contentPlaybackContext: {
+                    vis: 0,
+                    splay: false,
+                    lactMilliseconds: "-1",
+                    signatureTimestamp: innertube.session.player?.sts,
+                },
+            },
+            videoId,
+        };
+
+        if (poToken) {
+            params.serviceIntegrityDimensions = {};
+            params.serviceIntegrityDimensions.poToken = poToken;
+        }
+
+        const playerRequestJson = {
+            context: clonedInnerTubeContext,
+            ...params,
+        };
 
         const headers = [
             {
