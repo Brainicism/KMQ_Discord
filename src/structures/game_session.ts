fc597b1e715f602cd32b8cf9fbef45f099dce624
Taaha Mahdi <mahditaaha11@gmail.com>
2026-04-29 21:47:47 -0700

    prettier

--- src/structures/game_session.ts
+++ src/structures/game_session.ts
@@ -477,9 +477,7 @@ export default class GameSession extends Session {
             const levelUpMessages = leveledUpPlayers
                 .sort((a, b) => {
                     const levelsGainedDiff =
-                        b.endLevel -
-                        b.startLevel -
-                        (a.endLevel - a.startLevel);
+                        b.endLevel - b.startLevel - (a.endLevel - a.startLevel);
                     return levelsGainedDiff !== 0
                         ? levelsGainedDiff
                         : b.endLevel - a.endLevel;
